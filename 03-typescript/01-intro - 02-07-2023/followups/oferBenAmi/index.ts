8e02a17f820c0f40a4bb68a605d49a8d5892e945
OferBenAmi <benamiofer1@gmail.com>
2023-07-02 20:11:15 +0300

    Ts works

---
+++ 03-typescript/01-intro - 02-07-2023/followups/oferBenAmi/index.ts
@@ -0,0 +1,7 @@
+let counter = 0;
+let obj = {age: 24, job: "programmer", yearsOfExp: 4};
+if(obj.age >= 24 && obj.job == "programmer" && obj.yearsOfExp >=3 ) console.log("He can work here");
+else console.log("He cannot work here");
+for(let i = 1;i<=100 ; i++){
+  if (i%2 == 0) console.log(i);
+}
