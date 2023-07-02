6887f7ce1774818c8a13bd1d1f87d30e68b81ba7
uv1908 <yuval.lavi@avilavi.net>
2023-07-02 20:30:45 +0300

    yuval followup

---
+++ 03-typescript/01-intro - 02-07-2023/followups/Yuval/index.ts
@@ -0,0 +1,8 @@
+let firstName: string = 'Yuval';
+let age: number = 23;
+let userDetails = {
+    userName: 'uv1908',
+    emailAddress: 'yuval@gmail.com'
+};
+
+console.log(firstName, age, userDetails);
