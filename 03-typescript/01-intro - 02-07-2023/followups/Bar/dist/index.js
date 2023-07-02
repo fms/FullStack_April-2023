14cd8ceb0e780eee37448919b7c9e30fc3454a66
Bar .A <bar7569@gmail.com>
2023-07-02 20:33:16 +0300

    Some variables practice

---
+++ 03-typescript/01-intro - 02-07-2023/followups/Bar/dist/index.js
@@ -0,0 +1,25 @@
+var someText = "someText";
+var number = 5;
+var boolTrue = true;
+var boolFalse = false;
+var object = { firstName: "Someone", lastName: "Sometwo" };
+console.log(number + number);
+console.log(object.firstName);
+if (number != 5) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+if (object) {
+    console.log("nice");
+}
+else if (boolTrue) {
+    console.log("nope");
+}
+else if (boolFalse) {
+    console.log("oh");
+}
+else {
+    console.log(":(");
+}
---
+++ 03-typescript/01-intro - 02-07-2023/followups/Bar/index.ts
@@ -0,0 +1,29 @@
+let someText = "someText"
+let number = 5
+let boolTrue = true
+let boolFalse = false
+let object = { firstName: "Someone", lastName: "Sometwo" }
+
+console.log(number + number);
+console.log(object.firstName);
+
+if (number != 5) {
+    console.log(true)
+}
+else {
+    console.log(false);
+}
+
+if (object) {
+    console.log("nice");
+
+} else if (boolTrue) {
+    console.log("nope");
+
+} else if (boolFalse) {
+    console.log("oh");
+
+} else {
+    console.log(":(");
+
+}
