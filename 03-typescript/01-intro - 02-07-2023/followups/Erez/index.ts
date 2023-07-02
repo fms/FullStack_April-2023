077abdc0435ee66f344b4c9fd41dbcb5a89cf0d2
ErezArgaman <134220479+ErezArgaman@users.noreply.github.com>
2023-07-02 20:20:28 +0300

    adding script

---
+++ 03-typescript/01-intro - 02-07-2023/followups/Erez/index.ts
@@ -0,0 +1,15 @@
+let userDetails =
+{
+    firstName: "erez",
+    firstName1: "avi",
+    lastName: "alfi",
+    lastName1: "opal",
+};
+
+if (userDetails.firstName) {
+    console.log('alfi');
+}
+
+if (userDetails.firstName1) {
+    console.log('opal');
+}
