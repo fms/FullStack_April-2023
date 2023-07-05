62ccabc44d386f155cd1ac4d4812cb4f446675ac
ashours <shlomiasus257@gmail.com>
2023-07-05 22:01:12 +0300

    improvment of message

--- 03-typescript/02-const, conditions - 05-07-2023/followup/shlomi asus/BMI/bmi.ts
+++ 03-typescript/02-const, conditions - 05-07-2023/followup/shlomi asus/BMI/bmi.ts
@@ -4,7 +4,9 @@ var weight: string = prompt("please insert your weight in numbers only:") || "0"
 var result: number | null = ((parseInt(weight) / parseInt(height)) ** 2) * 100;
 
 if (result >= 25) {
-    console.log("you are fat,your BMI is:", + result);
+    window.alert("you are fat,your BMI is:" + result);
 }
-else
-    console.log("you are lean,your BMI is:", + result);
+else {
+    window.alert("you are lean,your BMI is:" + result);
+}
+
