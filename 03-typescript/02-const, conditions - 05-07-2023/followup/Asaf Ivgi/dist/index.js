36a6fcb8268340bc8febdc319cdc7d387beb1acf
AsafIvgi <88776471+AsafIvgi@users.noreply.github.com>
2023-07-05 20:35:55 +0300

    more work

--- 03-typescript/02-const, conditions - 05-07-2023/followup/Asaf Ivgi/dist/index.js
+++ 03-typescript/02-const, conditions - 05-07-2023/followup/Asaf Ivgi/dist/index.js
@@ -1,5 +1,7 @@
-var userWeight = prompt("What is your weight in KG?: ");
-var userHeight = prompt("What is your height in meters?: ");
+var userWeight = prompt("What is your weight in KG?: ") || "0";
+var userHeight = prompt("What is your height in meters?: ") || "0";
+var height = parseInt(userHeight);
+var weight = parseInt(userWeight);
 var BMI = userWeight / userHeight ^ 2;
 if (BMI > 25) {
     console.log("Overweight");
