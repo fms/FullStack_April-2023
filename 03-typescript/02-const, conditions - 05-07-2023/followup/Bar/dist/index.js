1e546d35c28bbba43d8626b63eb4cfe351534827
Bar .A <bar7569@gmail.com>
2023-07-05 20:41:31 +0300

    BMI Calculator

--- 03-typescript/02-const, conditions - 05-07-2023/followup/Bar/dist/index.js
+++ 03-typescript/02-const, conditions - 05-07-2023/followup/Bar/dist/index.js
@@ -1,18 +1,34 @@
-var a = 5;
-var b = 3;
-if (a > b) {
-    console.log("The expression is true");
-}
-else {
-    console.log("The expression is false");
-}
-var height = prompt("Enter height");
-if (!Number.isNaN(height) && height) {
-    var heightAsNum = parseInt(height);
-    if (heightAsNum >= 150) {
-        console.log("Your are tall");
+// var a = 5
+// var b = 3
+// if (a > b) {
+//     console.log("The expression is true");
+// }
+// else {
+//     console.log("The expression is false");
+// }
+// var height = prompt("Enter height")
+// if (!Number.isNaN(height) && height) {
+//     var heightAsNum = parseInt(height);
+//     if (heightAsNum >= 150) {
+//         console.log("Your are tall");
+//     }
+//     else {
+//         console.log("You are short");
+//     }
+// }
+var weight = prompt("Enter Weight");
+var height = prompt("Enter Height");
+if (weight && height) {
+    var parsedWeight = parseInt(weight);
+    var parsedHeight = parseInt(height) / 100;
+    if (!Number.isNaN(parsedHeight) && !Number.isNaN(parsedWeight)) {
+        var bmi = Math.round(parsedWeight / (parsedHeight * parsedHeight));
+        console.log(bmi);
     }
     else {
-        console.log("You are short");
+        console.log("Invalid Text");
     }
+}
+else {
+    console.log("Invalid Text");
 }
--- 03-typescript/02-const, conditions - 05-07-2023/followup/Bar/index.ts
+++ 03-typescript/02-const, conditions - 05-07-2023/followup/Bar/index.ts
@@ -1,22 +1,38 @@
-var a = 5
-var b = 3
+// var a = 5
+// var b = 3
 
-if (a > b) {
-    console.log("The expression is true");
+// if (a > b) {
+//     console.log("The expression is true");
 
-}
-else {
-    console.log("The expression is false");
-}
+// }
+// else {
+//     console.log("The expression is false");
+// }
 
 
-var height = prompt("Enter height")
-if (!Number.isNaN(height) && height) {
-    var heightAsNum = parseInt(height);
-    if (heightAsNum >= 150) {
-        console.log("Your are tall");
+// var height = prompt("Enter height")
+// if (!Number.isNaN(height) && height) {
+//     var heightAsNum = parseInt(height);
+//     if (heightAsNum >= 150) {
+//         console.log("Your are tall");
+//     }
+//     else {
+//         console.log("You are short");
+//     }
+// }
+
+var weight = prompt("Enter Weight")
+var height = prompt("Enter Height")
+
+if (weight && height) {
+    var parsedWeight = parseInt(weight);
+    var parsedHeight = parseInt(height) / 100;
+    if (!Number.isNaN(parsedHeight) && !Number.isNaN(parsedWeight)) {
+        var bmi = Math.round(parsedWeight / (parsedHeight * parsedHeight));
+        console.log(bmi);
+    } else {
+        console.log("Invalid Text");
     }
-    else {
-        console.log("You are short");
-    }
+} else {
+    console.log("Invalid Text");
 }
