a1ab724b7aee296c7a44d4e5e5b5308859229f8e
ofer ben ami <benamiofer1@gmail.com>
2023-07-05 16:16:46 +0300

    Doing More TS

--- 03-typescript/01-intro - 02-07-2023/followups/oferBenAmi/index.ts
+++ 03-typescript/01-intro - 02-07-2023/followups/oferBenAmi/index.ts
@@ -1,7 +1,33 @@
-let counter = 0;
-let obj = {age: 24, job: "programmer", yearsOfExp: 4};
-if(obj.age >= 24 && obj.job == "programmer" && obj.yearsOfExp >=3 ) console.log("He can work here");
+let worker1 = {
+  age: 24,
+  job: "programmer",
+  yearsOfExp: 4,
+  isAvailableToWork: true,
+};
+if (
+  worker1.age >= 24 &&
+  worker1.job == "programmer" &&
+  worker1.yearsOfExp >= 3 &&
+  worker1.isAvailableToWork == true
+)
+  console.log("He can work here");
 else console.log("He cannot work here");
-for(let i = 1;i<=100 ; i++){
-  if (i%2 == 0) console.log(i);
-}
+
+
+
+const printEvens = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) console.log(i);
+  }
+};
+const printOdds = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 2 != 0) console.log(i);
+  }
+};
+
+
+const head1 = document.getElementById('head1');
+head1?.classList.add("head1-from-ts");
+head1?.classList.remove("head1");
+
