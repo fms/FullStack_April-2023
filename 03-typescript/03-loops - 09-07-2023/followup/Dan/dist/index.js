8399c2496df3eed55b3241c57a844ab5cdac2cb7
Danbn23 <danbn23@gmail.com>
2023-07-09 20:19:38 +0300

    Dan's BOOM

---
+++ 03-typescript/03-loops - 09-07-2023/followup/Dan/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let i = 1;
+while (i <= 100) {
+    if (i % 7 == 0) {
+        console.log("BOOM");
+    }
+    else {
+        console.log(i);
+    }
+    i++;
+}
---
+++ 03-typescript/03-loops - 09-07-2023/followup/Dan/index.ts
@@ -0,0 +1,13 @@
+let num: any = 1;
+
+while(num <= 100) {
+
+    if(num%7 == 0) {
+        console.log("BOOM")
+    } 
+    else {
+        console.log(num)
+    }
+    num ++;
+    
+}
