19e1397a4edcac958cbcfc8fd9f9e60e7c5c6071
ariel7123 <ari71232@outlook.com>
2023-07-09 17:48:33 +0300

    task #1 completed
    :+1:

--- 03-typescript/02-const, conditions - 05-07-2023/followup/Ariel.I/script.ts
+++ 03-typescript/02-const, conditions - 05-07-2023/followup/Ariel.I/script.ts
@@ -1,8 +1,21 @@
-const userWeight: any = prompt(`Please sir, what's your weight in Kg?`);
-const userHeight: any = prompt(`Please sir, what's your Height in Cm?`);
+const userWeight: string | null =
+  prompt(`Please sir, what's your weight in Kg? (for example -> 88)`) || "0";
+const userHeight: string | null =
+  prompt(`Please sir, what's your Height in Cm? (for example -> 1.84)`) || "0";
 
-const BMIClaculate = userWeight / (userHeight * userHeight);
+const BMIClaculate =
+  parseFloat(userWeight) / (parseFloat(userHeight) * parseFloat(userHeight));
 
-console.log(
-  `Well your BMI is ${BMIClaculate} 👌, drink water is hot today! 💧💧💧`
-);
+if (BMIClaculate > 25) {
+  console.log(
+    `Well your BMI is ${BMIClaculate} 👌, that mean's your in Overweight! \n drink water is hot today! 💧💧💧`
+  );
+} else if (BMIClaculate < 18) {
+  console.log(
+    `Well your BMI is ${BMIClaculate} 👌, that mean's your in Underweight! \n drink water is hot today! 💧💧💧`
+  );
+} else {
+  console.log(
+    `Well your BMI is ${BMIClaculate} 👌, your healthy! (normal) \n drink water is hot today! 💧💧💧`
+  );
+}
