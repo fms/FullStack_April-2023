ce1065dc56eb37c55e4a1da8e1126a2286aad16b
Alon Nachshon <alon0255@gmail.com>
2023-07-09 20:21:13 +0300

    update

--- 03-typescript/03-loops - 09-07-2023/followup/AlonNachshon/day2/index.ts
+++ 03-typescript/03-loops - 09-07-2023/followup/AlonNachshon/day2/index.ts
@@ -2,7 +2,7 @@ let i:number = 1;
 
 while(i<=100){
     if(i%7 == 0 || i%10 == 7 ||( i >=71 && i<80)){
-        console.log("BOOM")
+        console.log(i+" BOOM")
     }else{
         console.log(i)
     }
