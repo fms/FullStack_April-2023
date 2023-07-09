d212473326407f1129d600d5db697c2b61f82e42
Shmuel Fishman <fms@shmulik-fishman.com>
2023-07-09 18:22:10 +0300

    Added example for assignment & interpolation

---
+++ 03-typescript/03-loops - 09-07-2023/example-assignments/index.ts
@@ -0,0 +1,40 @@
+let firstNumber = 1;
+
+firstNumber = firstNumber + 1;
+console.log(firstNumber);
+
+firstNumber += 1;
+console.log(firstNumber);
+
+firstNumber++;
+console.log(firstNumber);
+
+firstNumber = firstNumber * 2;
+console.log(firstNumber);
+
+firstNumber **= 2;
+console.log(firstNumber);
+
+++firstNumber;   // firstNumber += 1;
+firstNumber++;   // temp = firstNumber; firstNumber +=1; temp;
+
+let initial = firstNumber;
+console.log('initial: ' + firstNumber);
+console.log('++firstNumber: ' + ++firstNumber );
+console.log('firstNumber++: ' + firstNumber++ );
+
+// interpolation. Both lines are the same.
+console.log('final' + firstNumber);
+console.log(`final: ${firstNumber}`);
+
+console.log('Summary: started with ' + initial + ', ended with ' + firstNumber);
+console.log(`Summary: started with ${initial}, ended with ${firstNumber}`);
+
+// Avoid these, if you can:
+firstNumber = 10;
+let result = ++firstNumber * ++firstNumber;
+console.log(result);
+
+firstNumber = 10;
+result = firstNumber++ * firstNumber++;
+console.log(result);
