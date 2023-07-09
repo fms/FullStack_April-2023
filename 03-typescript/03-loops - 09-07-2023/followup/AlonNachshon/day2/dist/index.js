9d3814636c720f7f0ebc94720702f0afc421430b
Alon Nachshon <alon0255@gmail.com>
2023-07-09 20:07:30 +0300

    finish

--- 03-typescript/03-loops - 09-07-2023/followup/AlonNachshon/day2/dist/index.js
+++ 03-typescript/03-loops - 09-07-2023/followup/AlonNachshon/day2/dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 let i = 1;
 while (i <= 100) {
-    if (i % 7 == 0 || i % 10 == 7 || i >= 71 && i < 80) {
+    if (i % 7 == 0 || i % 10 == 7 || (i >= 71 && i < 80)) {
         console.log("BOOM");
     }
     else {
--- 03-typescript/03-loops - 09-07-2023/followup/AlonNachshon/day2/index.ts
+++ 03-typescript/03-loops - 09-07-2023/followup/AlonNachshon/day2/index.ts
@@ -1,7 +1,7 @@
 let i:number = 1;
 
 while(i<=100){
-    if(i%7 == 0 || i%10 == 7 || i >=71 && i<80){
+    if(i%7 == 0 || i%10 == 7 ||( i >=71 && i<80)){
         console.log("BOOM")
     }else{
         console.log(i)
