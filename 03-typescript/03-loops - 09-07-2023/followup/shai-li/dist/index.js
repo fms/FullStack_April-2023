7ca7d2936a82874ac6d4aad120abdcccbdc2adab
Shai-li Harniv <shaili6298@gmail.com>
2023-07-09 21:30:04 +0300

    as

---
+++ 03-typescript/03-loops - 09-07-2023/followup/shai-li/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+//Play 7 boom.
+// - loop from 1 to 100
+// - print the number
+// - Whenever a number divides by 7 write 'BOOM', on the same line
+// - Advanced: write 'BOOM' if the number contains a 7.
+// let number=1
+for (let number = 1; number <= 100; number++) {
+    if (number % 7 === 0) {
+        console.log(`${number} BOOM`);
+    }
+    else {
+        console.log(number);
+    }
+}
