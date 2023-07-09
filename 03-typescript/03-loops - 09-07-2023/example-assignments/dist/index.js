ccfa6ca59062319b84e46c78423ab79c755d3ece
Shmuel Fishman <fms@shmulik-fishman.com>
2023-07-09 20:03:08 +0300

    Added some comments to the assignment example

--- 03-typescript/03-loops - 09-07-2023/example-assignments/dist/index.js
+++ 03-typescript/03-loops - 09-07-2023/example-assignments/dist/index.js
@@ -1,11 +1,14 @@
 "use strict";
+const startNUmber = 1;
 let firstNumber = 1;
+// The next 3 items are the same
 firstNumber = firstNumber + 1;
 console.log(firstNumber);
 firstNumber += 1;
 console.log(firstNumber);
 firstNumber++;
 console.log(firstNumber);
+// -----
 firstNumber = firstNumber * 2;
 console.log(firstNumber);
 firstNumber = Math.pow(firstNumber, 2);
@@ -17,7 +20,7 @@ console.log('initial: ' + firstNumber);
 console.log('++firstNumber: ' + ++firstNumber);
 console.log('firstNumber++: ' + firstNumber++);
 // interpolation. Both lines are the same.
-console.log('final' + firstNumber);
+console.log('final ' + firstNumber);
 console.log(`final: ${firstNumber}`);
 console.log('Summary: started with ' + initial + ', ended with ' + firstNumber);
 console.log(`Summary: started with ${initial}, ended with ${firstNumber}`);
