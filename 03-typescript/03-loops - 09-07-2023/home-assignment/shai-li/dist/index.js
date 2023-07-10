adbb1400b8c3353a193597e4cd14dd96db7ed765
Shai-li Harniv <shaili6298@gmail.com>
2023-07-10 12:01:45 +0300

    f

---
+++ 03-typescript/03-loops - 09-07-2023/home-assignment/shai-li/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// 1. Do you know the "99 Bottles of Beer on the wall" song?
+//    - Create a loop, counting back from 99 which prints:
+//    > 99 bottles of beer on the wall, 99 bottles of beer.<br>
+//    > Take one down and pass it around, now there's 98 more bottles of beer on the wall!<br>
+//    > <br>
+//    - When you reach 1, print:
+//    > 1 bottle of beer on the wall, 1 bottle of beer.<br>
+//    > Take one down and pass it around, there's no more bottles of beer on the wall!<br>
+//    > <br>
+//    - exit when there are no more bottles.
