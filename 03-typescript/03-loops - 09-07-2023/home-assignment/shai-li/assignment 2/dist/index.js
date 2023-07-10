7818af95ddecdda20254be15c091a41d101d407d
Shai-li Harniv <shaili6298@gmail.com>
2023-07-10 12:40:47 +0300

    more

--- 03-typescript/03-loops - 09-07-2023/home-assignment/shai-li/assignment 2/dist/index.js
+++ 03-typescript/03-loops - 09-07-2023/home-assignment/shai-li/assignment 2/dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 //2. Get a number (n) from the user.
 // - Calculate and print the total: 1+2+3+...+n.
 // - Use a loop and not the formula for this arithmetic sequence.
--- 03-typescript/03-loops - 09-07-2023/home-assignment/shai-li/assignment 3/dist/index.js
+++ 03-typescript/03-loops - 09-07-2023/home-assignment/shai-li/assignment 3/dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 // 3. Get a number (n) from the user.
 //     - Use nested loops and calculate the total of 1+2+...+n for the series of length 1 to n. (1 + 1+2 + 1+2+3 + ... + 1+2+...+n).
 //     - Use an outer loop from 1 to n (outer)
