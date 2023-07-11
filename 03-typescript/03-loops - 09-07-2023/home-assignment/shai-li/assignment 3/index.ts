24d512d00a0635bf1f01a40f367aee79c31952e8
Shai-li Harniv <shaili6298@gmail.com>
2023-07-11 19:04:13 +0300

    j

--- 03-typescript/03-loops - 09-07-2023/home-assignment/shai-li/assignment 3/index.ts
+++ 03-typescript/03-loops - 09-07-2023/home-assignment/shai-li/assignment 3/index.ts
@@ -2,3 +2,18 @@
 //     - Use nested loops and calculate the total of 1+2+...+n for the series of length 1 to n. (1 + 1+2 + 1+2+3 + ... + 1+2+...+n).
 //     - Use an outer loop from 1 to n (outer)
 //     - Use an inner loop for the 1+2+...outer (again, loop, not formula)
+
+let getNumber: string | null = prompt("Give me a number please");
+
+if (getNumber) {
+  let num = parseInt(getNumber);
+  console.log(num);
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+    console.log(sum);
+  }
+  if (Number.isNaN(num)) {
+    console.log("You didn't gave me number!");
+  }
+}
