e5f6316d8e9f227c5e854f8d2168c8eb7a814449
Bar .A <bar7569@gmail.com>
2023-07-11 18:49:09 +0300

    Added assignments

---
+++ 03-typescript/03-loops - 09-07-2023/home-assignment/Bar/Assignment/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+let bottles = 99;
+while (bottles <= 99 && bottles > 0) {
+    if (bottles === 1) {
+        console.log("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, there are no more bottles of beer on the wall!");
+    }
+    else {
+        console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of beer. Take one down and pass it around, now there are ${bottles - 1} bottles of beer on the wall!`);
+    }
+    bottles--;
+}
