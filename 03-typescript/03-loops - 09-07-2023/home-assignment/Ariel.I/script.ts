da9b929368bbbd968c0f3383e220fdc2ebe47b41
ariel7123 <ari71232@outlook.com>
2023-07-12 20:42:10 +0300

    just commented...nothing changed by me

--- 03-typescript/03-loops - 09-07-2023/home-assignment/Ariel.I/script.ts
+++ 03-typescript/03-loops - 09-07-2023/home-assignment/Ariel.I/script.ts
@@ -1,17 +1,17 @@
 // mission #1
-for (let bottles = 99; bottles >= 1; bottles--) {
-  let bottleDown = bottles - 1;
-  if (bottles != 1) {
-    console.log(
-      `${bottles} bottles of beer on the wall, ${bottles} bottles of beer.
-       \n Take one down and pass it around, now there's ${bottleDown} more bottles of beer on the wall!`
-    );
-  } else {
-    console.log(
-      `${bottles} bottle of beer on the wall, ${bottles} bottle of beer. \n Take one down and pass it around, there's no more bottles of beer on the wall!`
-    );
-  }
-}
+// for (let bottles = 99; bottles >= 1; bottles--) {
+//   let bottleDown = bottles - 1;
+//   if (bottles != 1) {
+//     console.log(
+//       `${bottles} bottles of beer on the wall, ${bottles} bottles of beer.
+//        \n Take one down and pass it around, now there's ${bottleDown} more bottles of beer on the wall!`
+//     );
+//   } else {
+//     console.log(
+//       `${bottles} bottle of beer on the wall, ${bottles} bottle of beer. \n Take one down and pass it around, there's no more bottles of beer on the wall!`
+//     );
+//   }
+// }
 
 // Mission #2
 // let numberFromUser = parseInt(
@@ -27,17 +27,17 @@ for (let bottles = 99; bottles >= 1; bottles--) {
 // );
 
 // Mission #3
-// let numberChoosenByUser = parseInt(
-//   prompt(`Let's choose again a number!`) || "0"
-// );
-// let total1 = 0;
+let numberChoosenByUser = parseInt(
+  prompt(`Let's choose again a number!`) || "0"
+);
+let total1 = 0;
 
-// for (let loopCuonter = 1; loopCuonter <= numberChoosenByUser; loopCuonter++) {
-//   for (let loopCalculate = 1; loopCalculate <= loopCuonter; loopCalculate++) {
-//     total1 = total1 + loopCalculate;
-//   }
-// }
+for (let loopCuonter = 1; loopCuonter <= numberChoosenByUser; loopCuonter++) {
+  for (let loopCalculate = 1; loopCalculate <= loopCuonter; loopCalculate++) {
+    total1 = total1 + loopCalculate;
+  }
+}
 
-// console.log(
-//   `Well if we took your number (${numberChoosenByUser}) and start plus from 1 to your number ${numberChoosenByUser}, we got summery of ${total1} 😀`
-// );
+console.log(
+  `Well if we took your number (${numberChoosenByUser}) and start plus from 1 to your number ${numberChoosenByUser}, we got summery of ${total1} 😀`
+);
