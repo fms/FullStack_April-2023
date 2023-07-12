153f592015d1d04da94149858f96adf617ed2c9f
israelcohen1 <israel46717@gmail.com>
2023-07-13 01:13:56 +0300

    add exercise 99 Bottles

---
+++ 03-typescript/03-loops - 09-07-2023/home-assignment/israel/index.ts
@@ -0,0 +1,4 @@
+for( let Bottles = 99; Bottles>1; Bottles--){
+    console.log(`${Bottles} bottles of beer on the wall,${Bottles} bottles of beer, Take one down and pass it around, now there's ${Bottles-1} more bottles of beer on the wall!`);
+}
+console.log("1 bottle of beer on the wall, 1 bottle of beer, Take one down and pass it around, there's no more bottles of beer on the wall!")
