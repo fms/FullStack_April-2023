b9f04f3cc8b16861e2e3b912dbb8f851d1b8b87b
ofer ben ami <benamiofer1@gmail.com>
2023-07-13 16:03:11 +0300

    Functions Training

---
+++ 03-typescript/04-functions - 12-07-2023/followup/OferBenAmi/index.ts
@@ -0,0 +1,14 @@
+function addTwoNum(a:number, b:number):number{
+  return a+b;
+}
+function square(a:number):number{
+  return a*a;
+}
+const bashlishit = (num:number):number => num*num*num;
+const random1To10 = ():number => Math.floor(Math.random()*10+1);
+
+console.log(square(4));
+
+console.log(bashlishit(4));
+
+console.log(random1To10());
