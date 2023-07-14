27e3048b103211f5a145b1cf7ee39350fce97512
uv1908 <yuval.lavi@avilavi.net>
2023-07-14 13:33:56 +0300

    improvements

--- 03-typescript/04-functions - 12-07-2023/home-assignment/Yuval/3rd/dist/index.js
+++ 03-typescript/04-functions - 12-07-2023/home-assignment/Yuval/3rd/dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
 function temperatureConvert(f) {
-    let c = (f - 32) * 5 / 9;
-    return c;
+    // let c = (f - 32) * 5 / 9;
+    // return c;
+    return (f - 32) * 5 / 9;
 }
 console.log(`32F = ${temperatureConvert(32)}C`);
 console.log(`100F = ${temperatureConvert(100)}C`);
