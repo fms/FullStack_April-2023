23872efdfd75d95fd1ed12a9da1089cd7fb79594
yelenaHayat <yelenamilhman@gmail.com>
2023-07-15 15:47:39 +0300

    homework update

--- 03-typescript/03-loops - 09-07-2023/home-assignment/yelena/99 Bottles of Beer/index.ts
+++ 03-typescript/03-loops - 09-07-2023/home-assignment/yelena/99 Bottles of Beer/index.ts
@@ -1,4 +1,5 @@
 
+/*99 Bottles of Beer*/
 
 for(let bottleCount = 99 ; bottleCount > 0 ; bottleCount--)
 {
@@ -14,3 +15,55 @@ for(let bottleCount = 99 ; bottleCount > 0 ; bottleCount--)
     );
   }
 }
+
+/*Get a number (n) from the user.
+
+Calculate and print the total: 1+2+3+...+n.
+Use a loop and not the formula for this arithmetic sequence.*/
+
+/*let userInput = parseInt(prompt('Please Write some numbers to calculate the total');
+let sum = 0;
+
+if(userInput <= 0){
+  
+}
+sum+=userInput;*/
+
+let userInput = parseInt(prompt("Please Enter numbers for the total calculation") || "0");
+let sum = 0;
+
+for (let i = 0; i <= userInput; i++){
+
+  if (!Number.isNaN(userInput)) {
+    
+     sum+=userInput;
+  }
+  else {
+    console.log("its  not a number");
+  }
+}
+
+/*Get a number (n) from the user.
+
+Use nested loops and calculate the total of 1+2+...+n for the series of length 1 to n. (1 + 1+2 + 1+2+3 + ... + 1+2+...+n).
+Use an outer loop from 1 to n (outer)
+Use an inner loop for the 1+2+...outer (again, loop, not formula)*/
+
+let totalOfTotal = 0
+
+if (!Number.isNaN(userInput)) {
+    for (let i = 0; i <= userInput; i++) {
+        sum += i
+        for (let j = 0; j <= i; j++) {
+            totalOfTotal += j
+        }
+    }
+    console.log(sum);
+    console.log(totalOfTotal);
+}
+else{
+    console.log("Invalid value");
+}
+
+
+
