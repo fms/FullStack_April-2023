7dfc9777dd38d990bccf9a5284254bcac263ab7e
Shmuel Fishman <fms@shmulik-fishman.com>
2023-07-16 20:57:08 +0300

    Add example for trinary operator

---
+++ 03-typescript/05-triany op, switch, enum, break, continue - 16-07-2023/example-trinary/assignment-1.ts
@@ -0,0 +1,9 @@
+function negativeWithVar(num: number) : number {
+    let result :number = -num;
+    return result;
+}
+
+function negativeWithoutVar(num: number) : number {
+    return -num;
+}
+
---
+++ 03-typescript/05-triany op, switch, enum, break, continue - 16-07-2023/example-trinary/dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+let input = 5;
+console.log(`The number ${input} is ${input % 2 === 0 ? "even" : "odd"}`);
+// This is the same as:
+if (input % 2 === 0) {
+    console.log(`The number ${input} is even`);
+}
+else {
+    console.log(`The number ${input} is odd`);
+}
+// Nested trinary operator
+console.log(`The number ${input} divides by ${input % 3 === 0 ? (input % 5 === 0 ? "15" : "3") : "something else"}`);
+// This is equivelent to 
+let dividorAsString;
+if (input % 3 === 0) { // Outer trinary
+    if (input % 5 === 0) { // Inner trinary - true of outer trinary
+        dividorAsString = "15"; // True for inner trinary
+    }
+    else {
+        dividorAsString = "3"; // False for inner trinary
+    }
+}
+else {
+    dividorAsString = "something else"; // False of outer trinary
+}
+console.log(`The number ${input} divides by ${dividorAsString}`);
