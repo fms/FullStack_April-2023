1c567693c6eab1c4676f4be934397d874f36a71d
uv1908 <yuval.lavi@avilavi.net>
2023-07-16 18:38:40 +0300

    big improvements

--- 03-typescript/04-functions - 12-07-2023/home-assignment/Yuval/1st/dist/index.js
+++ 03-typescript/04-functions - 12-07-2023/home-assignment/Yuval/1st/dist/index.js
@@ -2,7 +2,7 @@
 function negative(num) {
     // let negativeNum: number;
     // return (negativeNum = num * -1);
-    return num *= -1;
+    return -num;
 }
 console.log(`Original number: 5, negated number: ${negative(5)}`);
 console.log(`Original number: -21, negated number: ${negative(-21)}`);
