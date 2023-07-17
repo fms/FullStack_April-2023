be353796ebcab3e0b28ed5dd8707260dfd1a127f
Bar .A <bar7569@gmail.com>
2023-07-17 18:15:23 +0300

    Switch and case added

--- 03-typescript/04-functions - 12-07-2023/home-assignment/Bar/Function4/Function3/index.ts
+++ 03-typescript/04-functions - 12-07-2023/home-assignment/Bar/Function4/Function3/index.ts
@@ -21,7 +21,39 @@ function operationOnNumber(num: number, num2: number, op: number) {
         }
 }
 
-operationOnNumber(2, 2, 1)
-operationOnNumber(2, 2, 2)
-operationOnNumber(2, 2, 3)
-operationOnNumber(2, 2, 4)
+// operationOnNumber(2, 2, 1)
+// operationOnNumber(2, 2, 2)
+// operationOnNumber(2, 2, 3)
+// operationOnNumber(2, 2, 4)
+
+enum operator {
+        add = 1,
+        substruct = 2,
+        multiply = 3,
+        divide = 4
+}
+
+function opreationWithSwitch(num: number, num2: number, op: number) {
+        let result = 0
+        switch (op) {
+                case operator.add:
+                        result = num + num2
+                        break
+                case operator.substruct:
+                        result = num - num2
+                        break
+                case operator.multiply:
+                        result = num * num2
+                        break
+                case operator.divide:
+                        result = num / num2
+
+                default: NaN
+        }
+        return result
+}
+
+opreationWithSwitch(2, 2, operator.add)
+opreationWithSwitch(2, 2, operator.substruct)
+opreationWithSwitch(2, 2, operator.multiply)
+opreationWithSwitch(2, 2, operator.divide)
