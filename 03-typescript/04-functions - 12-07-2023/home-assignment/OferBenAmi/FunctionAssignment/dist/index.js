08cc6ca756e7deabf733d2532e435ed567a6c380
OferBenAmi <benamiofer1@gmail.com>
2023-07-18 03:42:29 +0300

    Doing for loops and function training

--- 03-typescript/04-functions - 12-07-2023/home-assignment/OferBenAmi/FunctionAssignment/dist/index.js
+++ 03-typescript/04-functions - 12-07-2023/home-assignment/OferBenAmi/FunctionAssignment/dist/index.js
@@ -26,7 +26,28 @@ function MathSelect(num1, num2, op) {
         }
     }
 }
-console.log(negateNum(-50));
-console.log(largerNum(10, 20));
-console.log(fahrenheitToCelsius(100));
-console.log(MathSelect(3, 5, 6));
+function biggestUpCount(n) {
+    var privNum = parseInt(prompt("Please enter the first number") || "0");
+    var biggetCount = 0;
+    var currentCount = 1;
+    for (var i = 1; i < n; i++) {
+        var currNum = parseInt(prompt("Please enter the " + (i + 1) + "th number") || "0");
+        if (currNum > privNum) {
+            currentCount++;
+            console.log("the current count is" + currentCount);
+            if (currentCount > biggetCount) {
+                biggetCount = currentCount;
+                console.log("the biggest count is" + biggetCount);
+            }
+            else {
+                currentCount = 0;
+            }
+        }
+    }
+    return biggetCount;
+}
+// console.log(negateNum(-50));
+// console.log(largerNum(10, 20));
+// console.log(fahrenheitToCelsius(100));
+// console.log(MathSelect(3, 5, 6));
+console.log(biggestUpCount(9));
