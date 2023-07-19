aff9d01996554ff217192302a65e52b4c6fe0bda
Saarxp <saar772@gmail.com>
2023-07-19 21:46:17 +0300

    06-assignments-saar

--- 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/Saar/dist/index.js
+++ 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/Saar/dist/index.js
@@ -2,11 +2,23 @@
 // Write a method for creating a new Car object.
 // Properties include: maker, model, year, milage, fuel consumption (km/l)
 // Add a method for calculating total fuel consumption (milage / fuel consunmption)
-let bugatti = carObjects(`Bugatti`, `Divo`, 2018, 75000, 50.0007232);
+;
+let bugatti = {
+    maker: `Bugatti`,
+    model: `Divo`,
+    year: 2018,
+    milage: 75000,
+    fuelConsunmption: 50.0007232,
+    totalFuelConsumtion() {
+        return (this.milage / this.fuelConsunmption);
+    }
+};
+console.log(bugatti);
+console.log(bugatti.totalFuelConsumtion());
 function carObjects(maker, model, year, milage, fuelConsunmption) {
     let totalFuelConsumtion = (milage / fuelConsunmption);
     let carProperties = `Car maker: ${maker}, Car model: ${model}, Car year: ${year}, Car milage ${milage}, Car fuelConsunmption ${fuelConsunmption}.`;
     return `${carProperties}, your total Fuel Consumtion is ${totalFuelConsumtion}`;
 }
+;
 console.log(carObjects('Mazda', 'Mazda 3', 2023, 100000, 60.008388));
-console.log(bugatti);
--- 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/Saar/index.ts
+++ 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/Saar/index.ts
@@ -9,9 +9,22 @@ interface car
     year :number,
     milage :number,
     fuelConsunmption : number,
+    totalFuelConsumtion : Function,
+};
+
+let bugatti :car = {
+    maker: `Bugatti`,
+    model:`Divo`,
+    year: 2018,
+    milage: 75000,
+    fuelConsunmption: 50.0007232,
+    totalFuelConsumtion () {
+        return (this.milage / this.fuelConsunmption)
+    }
 }
 
-let bugatti = carObjects(`Bugatti` ,`Divo` ,2018 ,75000 ,50.0007232)
+console.log(bugatti)
+console.log(bugatti.totalFuelConsumtion());
 
 
 function carObjects(maker :string , model :string, year :number, milage :number , fuelConsunmption :number)
@@ -19,8 +32,6 @@ function carObjects(maker :string , model :string, year :number, milage :number 
     let totalFuelConsumtion :number = (milage / fuelConsunmption)
     let carProperties :any = `Car maker: ${maker}, Car model: ${model}, Car year: ${year}, Car milage ${milage}, Car fuelConsunmption ${fuelConsunmption}.`;
     return `${carProperties}, your total Fuel Consumtion is ${totalFuelConsumtion}`
-}
+};
 
-console.log(carObjects('Mazda', 'Mazda 3', 2023 , 100000 , 60.008388))
-
-console.log(bugatti)
+console.log(carObjects('Mazda', 'Mazda 3', 2023 , 100000 , 60.008388));
