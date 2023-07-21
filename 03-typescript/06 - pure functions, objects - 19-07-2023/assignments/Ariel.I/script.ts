c3cd322e29fc376788bb8d6fe8eb73ce2559af47
ariel7123 <ari71232@outlook.com>
2023-07-21 18:38:53 +0300

    progress from class

---
+++ 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/Ariel.I/script.ts
@@ -0,0 +1,11 @@
+interface carNew {
+  maker: string;
+  model: string;
+  year: number;
+  milage: number;
+  fuelConsumption: Function;
+}
+
+function fuelConsumption(milage: number, fuelConsumption: number) {
+  return;
+}
