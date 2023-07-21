b5d69b053a4d6cdceb919bcf65f16fd243fbde90
israelcohen1 <israel46717@gmail.com>
2023-07-21 06:10:47 +0300

    99 botttles of beer

--- 03-typescript/03-loops - 09-07-2023/home-assignment/israel/dist/index.js
+++ 03-typescript/03-loops - 09-07-2023/home-assignment/israel/dist/index.js
@@ -1,4 +1,9 @@
-for (var Bottles = 99; Bottles > 1; Bottles--) {
-    console.log(Bottles + " bottles of beer on the wall," + Bottles + " bottles of beer, Take one down and pass it around, now there's " + (Bottles - 1) + " more bottles of beer on the wall!");
+// for( let Bottles = 99; Bottles>1; Bottles--){
+//     console.log(`${Bottles} bottles of beer on the wall,${Bottles} bottles of beer, Take one down and pass it around, now there's ${Bottles-1} more bottles of beer on the wall!`);
+// }
+// console.log("1 bottle of beer on the wall, 1 bottle of beer, Take one down and pass it around, there's no more bottles of beer on the wall! exit when there are no more bottles")
+var an = prompt("giv a number");
+for (var x = 1, y = 2; y <= an; y++) {
+    x = x + y;
 }
-console.log("1 bottle of beer on the wall, 1 bottle of beer, Take one down and pass it around, there's no more bottles of beer on the wall!");
+console.log(x);
