9a62f90c6b27d024ad8c96cf835c7d16912f9fb0
avielosweb <avielosweb@gmail.com>
2023-07-23 19:24:43 +0300

    aviel function-homework
    aviel function-homework

---
+++ 03-typescript/04-functions - 12-07-2023/home-assignment/Aviel/‏‏‏‏homwork3/index.ts
@@ -0,0 +1,9 @@
+function  convert_temperature   (f : number ) :number
+
+   
+{
+    return (f - 32) * 5 / 9;
+}
+
+console.log(`the Fahrenheit_temperature is 24, the Celsius temperature is: ${convert_temperature(24) }`);
+console.log(`the Fahrenheit_temperature is 50, the Celsius temperature is: ${convert_temperature(50) }`);
