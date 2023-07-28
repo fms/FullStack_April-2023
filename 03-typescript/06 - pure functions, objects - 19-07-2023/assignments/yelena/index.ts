91ebc9d33b8d7d0352753888c49f27fb30bf653c
yelenaHayat <yelenamilhman@gmail.com>
2023-07-28 09:08:37 +0300

    class work

---
+++ 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/yelena/index.ts
@@ -0,0 +1,17 @@
+/*Write a method for creating a new Car object.
+Properties include: maker, model, year, milage, fuel consumption (km/l)
+Add a method for calculating total fuel consumption (milage / fuel consunmption)*/
+
+
+interface car {
+    maker: String,
+    model: String,
+    year: number,
+    milage: number,
+    fuelConsumption: number,
+}
+
+let car1: car{
+
+    
+}
