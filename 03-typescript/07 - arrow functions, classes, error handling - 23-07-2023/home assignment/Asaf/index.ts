ed0d2d6798122981ecb3a54f5ee8bbe86c5eae76
AsafIvgi <88776471+AsafIvgi@users.noreply.github.com>
2023-07-30 16:37:43 +0300

    Upload easy level

---
+++ 03-typescript/07 - arrow functions, classes, error handling - 23-07-2023/home assignment/Asaf/index.ts
@@ -0,0 +1,76 @@
+class Board{
+    xValue: number;
+    yValue: number;
+
+    constructor (width: number, height: number) {
+        this.xValue = width;
+        this.yValue = height;
+    }
+}
+
+class Rook{
+    currentX: number;
+    currentY: number;
+
+    constructor(board: object, startX: number, startY: number) {
+        if (board.xValue >= startX >= 1 && board.yValue >= startY >= 1) {
+            this.currentX = startX;
+            this.currentY = startY;
+        } else {
+            console.log("Out of board borders.")
+            this.currentX = NaN;
+            this.currentY = NaN;
+        }
+    }
+
+    getLocation() {
+        console.log(`The rook's location is ${this.currentX} on the X axis and ${this.currentY} on the Y axis.`)
+    }
+    // borderCheck(){
+    //     if 
+    // }
+
+    goRight(steps: number) {
+        this.currentX += steps;
+        this.getLocation()
+    }
+    goLeft(steps: number) {
+        this.currentX -= steps;
+        this.getLocation()
+    }
+    goUp(steps: number) {
+        this.currentY += steps;
+        this.getLocation()
+    }
+    goDown(steps: number) {
+        this.currentY -= steps;
+        this.getLocation()
+    }
+}
+
+class Bishop extends Rook{
+    constructor(board: object, startX: number, startY: number) {
+        super(board, startX, startY)
+    }
+
+    goRightUp(steps: number) {
+        this.currentX += steps;
+        this.goUp(steps);
+    }
+    goLeftUp(steps: number) {
+        this.currentX -= steps;
+        this.goUp(steps);
+    }
+    goRightDown(steps: number) {
+        this.currentX += steps;
+        this.goDown(steps);
+    }
+    goLeftDown(steps: number) {
+        this.currentX -= steps;
+        this.goDown(steps);
+    }
+}
+
+// let board1 = new Board(500, 500);
+// let rook1 = new Rook(board1, 250, 250);
+// let bishop1 = new Bishop(board1, 200, 200);
