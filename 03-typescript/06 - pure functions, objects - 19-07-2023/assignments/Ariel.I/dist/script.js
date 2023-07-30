cb64cc5b449d30a89a69cfba564e798238c109a3
ariel7123 <ari71232@outlook.com>
2023-07-30 17:12:32 +0300

    #2

--- 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/Ariel.I/dist/script.js
+++ 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/Ariel.I/dist/script.js
@@ -1,4 +1,28 @@
 "use strict";
-function fuelConsumption(milage, fuelConsumption) {
-    return;
-}
+const creatorCar = (maker, model, hatchback, year, milage, fuelConsumption) => {
+    let newCarOfer = {
+        maker: maker,
+        model: model,
+        hatchback: hatchback,
+        year: year,
+        milage: milage,
+        fuelConsumption: fuelConsumption,
+        totalFuelConsumption: function () {
+            return this.milage / this.fuelConsumption;
+        },
+    };
+    return newCarOfer;
+};
+const ArielsBride = creatorCar("Honda", "Civic", true, 2011, 148, 12.6);
+console.log(ArielsBride);
+// const ArielsBride: carNames = {
+//   maker: "Honda",
+//   model: "civic",
+//   hatchback: true,
+//   year: 2014,
+//   milage: 148000,
+//   fuelConsumption: 12.9,
+//   totalFuelConsumption: function () {
+//     return this.milage / this.fuelConsumption;
+//   },
+// };
--- 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/Ariel.I/script.ts
+++ 03-typescript/06 - pure functions, objects - 19-07-2023/assignments/Ariel.I/script.ts
@@ -1,11 +1,81 @@
-interface carNew {
+// interface carNames {
+//   maker: string;
+//   model: string;
+//   hatchback: boolean;
+//   year: number;
+//   milage: number;
+//   fuelConsumption: number;
+//   totalFuelConsumption: Function;
+// }
+
+// const creatorCar = (
+//   maker: string,
+//   model: string,
+//   hatchback: boolean,
+//   year: number,
+//   milage: number,
+//   fuelConsumption: number
+// ): any => {
+//   let newCarOfer: carNames = {
+//     maker: maker,
+//     model: model,
+//     hatchback: hatchback,
+//     year: year,
+//     milage: milage,
+//     fuelConsumption: fuelConsumption,
+//     totalFuelConsumption: function () {
+//       return this.milage / this.fuelConsumption;
+//     },
+//   };
+//   return newCarOfer;
+// };
+
+// const ArielsBride = creatorCar("Honda", "Civic", true, 2011, 148, 12.6);
+// console.log(ArielsBride);
+
+// const ArielsBride: carNames = {
+//   maker: "Honda",
+//   model: "civic",
+//   hatchback: true,
+//   year: 2014,
+//   milage: 148000,
+//   fuelConsumption: 12.9,
+//   totalFuelConsumption: function () {
+//     return this.milage / this.fuelConsumption;
+//   },
+// };
+
+// attempt #2
+interface carProps {
   maker: string;
   model: string;
+  hatcback?: boolean;
   year: number;
   milage: number;
-  fuelConsumption: Function;
+  fuelConsumption: number;
 }
 
-function fuelConsumption(milage: number, fuelConsumption: number) {
-  return;
+function carMethoda(
+  maker: string,
+  model: string,
+  hatcback: boolean,
+  year: number,
+  milage: number,
+  fuelConsumption: number,
+  totalFuelConsumptionCalc: Function
+) {
+  let carDetailsIn: any = {
+    maker,
+    model,
+    hatcback,
+    year,
+    milage,
+    fuelConsumption,
+    totalFuelConsumptionCalc() {
+      return this.milage / this.fuelConsumption;
+    },
+  };
+  return carDetailsIn;
 }
+let ArielsCar = carMethoda("Honda", "Civic", true, 2011, 148000, 13 / 100);
+console.log(ArielsCar);
