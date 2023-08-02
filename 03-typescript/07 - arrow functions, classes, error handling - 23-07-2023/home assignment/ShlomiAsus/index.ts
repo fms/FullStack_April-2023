d09aee5d4ad82d29f8e267cb1dd0d7834cc5167e
Shlomi Asus <shlomiasus257@gmail.com>
2023-08-02 18:30:59 +0300

    home assignment

---
+++ 03-typescript/07 - arrow functions, classes, error handling - 23-07-2023/home assignment/ShlomiAsus/index.ts
@@ -0,0 +1,45 @@
+// Write a class named Bishop that behaves in the same manner as the Rook.
+// Include these methods for moving on the board:
+// <strong>goRightUp()</strong>
+// <strong>goLeftUp()</strong>
+// <strong>goRightDown()</strong>
+// <strong>goLeftDown()</strong>
+// <hr>
+ //  48:26 שיעור אחרון
+// Medium
+// Write a class named Queen that behaves in the same manner as the Rook and Bishop combined.
+// <hr>
+
+// Write a class named King that behaves in the same manner as the Queen, but is limited to a single step.
+// <hr>
+
+// Advanced
+// Read up on Abstract classes.
+// Convert Rook, Bishop, Queen and King to use a new abstract class ChessPiece.
+
+
+// The board starts at coordinates 1,1. ??????????????????????????
+
+class Board {
+    private width: number;
+    private height: number;
+    constructor(width: number, height: number) {
+        this.width = width ?? 1;
+        this.height = height ?? 1;
+    }
+}
+let gameBoard = new Board(10, 10);
+
+class Rook {
+    new1: Board;
+    x: number;
+    y: number;
+    constructor(new1: Board, x: number, y: number) {
+        this.new1 = new1;
+        this.x = x;
+        this.y = y;
+    }
+    getLocation() {
+        console.log(`This is the location width:` + `${this.x}` + `and height:` + `${this.y}`);
+    }
+}
