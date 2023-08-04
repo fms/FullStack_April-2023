99326df4bc8b967f2e0710d3de317cfd855f9bbd
uv1908 <yuval.lavi@avilavi.net>
2023-08-04 17:14:51 +0300

    finished medium assignmet

--- 03-typescript/08-classes,arrays - 30-07-2023/home-assignment/Yuval/Medium/index.ts
+++ 03-typescript/08-classes,arrays - 30-07-2023/home-assignment/Yuval/Medium/index.ts
@@ -15,7 +15,6 @@ class Person {
     }
 
     set fullName(newName: string) {
-        // [this.lastName, this.firstName] = newName.split(" ");
         let splitName = newName.split(" ");
         this.firstName = splitName[1];
         this.lastName = splitName[0];
@@ -32,19 +31,20 @@ class SocialNetwork {
     }
 
     findFollower(fullName: string): boolean {
-        if(this.accountIdentifier.indexOf(fullName) == -1) {
+        if(followers.indexOf(fullName) == -1) {
             return false;
         }
         return true;
     }
 
     addFollower(person: Person) {
-        let fullName: string;
+        // let fullName: string;
         if(this.findFollower(person.fullName)) {
             console.log(`Already a follower.`);
         }
         else {
             followers.push(person.fullName);
+            console.log(person.fullName);
         }
     }
 
@@ -52,22 +52,51 @@ class SocialNetwork {
         let splitName = fullName.split(" ");
         let firstName = splitName[1];
         let lastName = splitName[0];
-        let person = new Person(firstName, lastName);
+        let person = new Person(firstName, lastName, "");
         if(this.findFollower(fullName)) {
-            followers.splice(this.accountIdentifier.indexOf(fullName), 1);
+            followers.splice(followers.indexOf(fullName), 1);
             console.log(person);
             return person;
         }
         return null;
     }
+
+    get followersList(): string[] {
+        return followers;
+    }
+
+    alphabetic(): string[] {
+        let alphabeticFollowers: string[] = followers.sort();
+        return alphabeticFollowers;
+    }
+
+    print() {
+        console.log(`${this.socialNetworkName}: ${this.alphabetic()}`);
+    }
 }
 
 let yuval = new Person("Yuval", "Lavi", "Actor");
-let twitter = new SocialNetwork("Twitter", "@yuval__lavi");
+let yuvalTwitter = new SocialNetwork("Twitter", "@yuval__lavi");
 
-yuval.fullName;
-twitter.addFollower(yuval);
-console.log(twitter);
-twitter.removeFollower(`Lavi Yuval`);
-console.log(twitter);
+let theRock = new Person("Dwayne", "Johnson", "Actor");
+let mjf = new Person("Max", "Friedman", "Wrestler");
+let jamesGunn = new Person("James", "Gunn", "Director");
+let ld77 = new Person("Luka", "Doncic", "Basketball player");
+
+
+yuvalTwitter.addFollower(theRock);
+yuvalTwitter.print();
+
+yuvalTwitter.addFollower(mjf);
+yuvalTwitter.print();
+
+yuvalTwitter.addFollower(ld77);
+yuvalTwitter.print();
+
+yuvalTwitter.addFollower(jamesGunn);
+yuvalTwitter.print();
+
+yuvalTwitter.removeFollower("Doncic Luka");
+console.log(ld77.fullName);
+yuvalTwitter.print();
 
