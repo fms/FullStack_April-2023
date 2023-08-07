eec0e29f9a45b23303aef17773ec9da8c4a15370
OferBenAmi <benamiofer1@gmail.com>
2023-08-07 21:07:02 +0300

    Finishing Assignment 8 medium

--- 03-typescript/08-classes,arrays - 30-07-2023/home-assignment/OferBenAmi/dist/index.js
+++ 03-typescript/08-classes,arrays - 30-07-2023/home-assignment/OferBenAmi/dist/index.js
@@ -19,17 +19,37 @@ class SocialNetwork {
         this.followers = [];
     }
     addFollower(person) {
-        if (this.followers.includes(person.fullName)) {
+        if (this.followers.includes(person)) {
             return;
         }
-        this.followers.push(person.fullName);
+        this.followers.push(person);
     }
     removeFollower(fullName) {
-        if (this.followers.includes(fullName)) {
-            this.followers.splice(this.followers.indexOf(fullName), 1); // removes the first accurance of fullName
+        const removedFollower = this.followers.find(person => person.fullName === fullName);
+        if (removedFollower) {
+            this.followers.splice(this.followers.indexOf(removedFollower), 1);
+            return removedFollower;
+        }
+        else {
+            return null;
         }
     }
+    print() {
+        let allNamesArray = [];
+        this.followers.forEach(x => allNamesArray.push(x.fullName));
+        console.log(`${this.SocialNetworkName}:  ${allNamesArray.sort()}`);
+    }
 }
-const Ofer = new Person(`Ofer`, `Ben-Ami`, `programmer`);
+const ofer = new Person(`Ofer`, `Ben-Ami`, `worker`);
+const roni = new Person(`Roni`, `ya`, `student`);
+const avi = new Person(`Avi`, `asdasd`, `dadaad`);
+const golan = new Person(`Golan`, `dididi`, `football player`);
 const oferSocialNetwork = new SocialNetwork(`instagram`, `ofer134`);
-console.log(Ofer.firstName);
+const roniSocialNetwork = new SocialNetwork(`instagram`, `roniYa`);
+const aviSocialNetwork = new SocialNetwork(`instagram`, `aviGO`);
+const golanSocialNetwork = new SocialNetwork(`instagram`, `golanha`);
+oferSocialNetwork.addFollower(roni);
+oferSocialNetwork.addFollower(ofer);
+oferSocialNetwork.addFollower(avi);
+oferSocialNetwork.addFollower(golan);
+console.log(oferSocialNetwork.print());
