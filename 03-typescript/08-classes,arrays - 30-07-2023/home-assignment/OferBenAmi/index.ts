b20e109ff49b6788040998f42c70e6cdd31689ad
ofer ben ami <benamiofer1@gmail.com>
2023-08-09 06:12:01 +0300

    starting DOM assignment

--- 03-typescript/08-classes,arrays - 30-07-2023/home-assignment/OferBenAmi/index.ts
+++ 03-typescript/08-classes,arrays - 30-07-2023/home-assignment/OferBenAmi/index.ts
@@ -1,12 +1,24 @@
 class Person{
   constructor(public firstName: string, public lastName:string, public genre?:string){
-
   }
   get fullName(): string{
     return `${this.firstName} ${this.lastName}`
   }
   set fullName(newName:string){
     [this.firstName, this.lastName] = newName.split(" ");
+  }
+}
+class Celeb extends Person{
+  constructor(firstName: string, lastName:string, genre?:string ){
+    super(firstName,lastName,genre );
+  }
+  celebsocialNetworks:SocialNetwork[] = [];
+
+  addFollower(socialNetwork:SocialNetwork, person: Person){
+    const isSN  = this.celebsocialNetworks.find(SN => SN.SocialNetworkName === socialNetwork.SocialNetworkName.toString())
+    if(isSN === undefined){
+      this.celebsocialNetworks.push(socialNetwork)
+    }
   }
 }
 
@@ -38,10 +50,13 @@ class SocialNetwork{
   }
 }
 
+
 const ofer = new Person(`Ofer`,`Ben-Ami`, `worker`);
-const roni = new Person(`Roni`,`ya`,`student`)
-const avi = new Person(`Avi`,`asdasd`,`dadaad`)
-const golan = new Person(`Golan`,`dididi`,`football player`)
+const roni = new Person(`Roni`,`ya`,`student`);
+const avi = new Person(`Avi`,`asdasd`,`dadaad`);
+const golan = new Person(`Golan`,`dididi`,`football player`);
+const famous = new Celeb(`famous`,`celeb`, `model`);
+const famusSocialNetwork = new SocialNetwork(`facebook`, `famus123`)
 const oferSocialNetwork = new SocialNetwork(`instagram`,`ofer134`);
 const roniSocialNetwork = new SocialNetwork(`instagram`,`roniYa`);
 const aviSocialNetwork = new SocialNetwork(`instagram`,`aviGO`);
@@ -51,6 +66,7 @@ oferSocialNetwork.addFollower(roni)
 oferSocialNetwork.addFollower(ofer)
 oferSocialNetwork.addFollower(avi)
 oferSocialNetwork.addFollower(golan)
-
+famous.addFollower(oferSocialNetwork,ofer)
+console.log(famous.celebsocialNetworks);
 
 console.log(oferSocialNetwork.print());
