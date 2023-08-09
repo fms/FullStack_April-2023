dcc62f951d479f9a697cef6a5b1c42d5e4fc787a
AviVert <avremi6@gmail.com>
2023-08-09 18:07:08 +0300

    Array printing fix

--- 03-typescript/08-classes,arrays - 30-07-2023/home-assignment/Avi/dist/index.js
+++ 03-typescript/08-classes,arrays - 30-07-2023/home-assignment/Avi/dist/index.js
@@ -1,59 +1,54 @@
-var Person = /** @class */ (function () {
-    function Person(firstName, lastName, genre) {
+"use strict";
+class Person {
+    constructor(firstName, lastName, genre) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.genre = genre;
     }
-    Object.defineProperty(Person.prototype, "fullName", {
-        get: function () {
-            return this.firstName + " " + this.lastName;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return Person;
-}());
-var SocialNetwork = /** @class */ (function () {
-    function SocialNetwork(SocialNetworkname, accountIdentifier) {
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+class SocialNetwork {
+    constructor(SocialNetworkname, accountIdentifier) {
         this.SocialNetworkname = SocialNetworkname;
         this.accountIdentifier = accountIdentifier;
         this.followers = new Array;
     }
-    SocialNetwork.prototype.addFollower = function (person) {
+    addFollower(person) {
         if (this.followers.includes(person)) {
-            console.log("You are already following");
+            console.log(`You are already following`);
         }
         else {
             this.followers.push(person);
         }
-    };
-    SocialNetwork.prototype.removeFollower = function (fullName) {
-        for (var i = 0; i < this.followers.length; i++) {
+    }
+    removeFollower(fullName) {
+        for (let i = 0; i < this.followers.length; i++) {
             if (this.followers[i].fullName == fullName) {
                 this.followers.splice(i);
-                console.log("The follower " + fullName + " was deleted");
+                console.log(`The follower ${fullName} was deleted`);
             }
         }
-    };
-    SocialNetwork.prototype.print = function () {
-        console.log(this.SocialNetworkname + ": ");
-    };
-    return SocialNetwork;
-}());
-var ShlomoArtzi = new Person("Shlomo", "Artzi", "Singer");
-var LionelMessi = new Person("Lionel", "Messi", "football");
-var TwitterShlomo = new SocialNetwork("Twitter", "@Shlomo");
-var instagramMessi = new SocialNetwork("instagram", "@Messi");
-var NewPerson3 = new Person("Eli", "David");
-var NewPerson4 = new Person("Eli", "Cohen");
-var NewPerson5 = new Person("Dani", "Dani");
+    }
+    print() {
+        console.log(`${this.SocialNetworkname}: `);
+    }
+}
+let ShlomoArtzi = new Person(`Shlomo`, `Artzi`, `Singer`);
+let LionelMessi = new Person(`Lionel`, `Messi`, `football`);
+let TwitterShlomo = new SocialNetwork(`Twitter`, `@Shlomo`);
+let instagramMessi = new SocialNetwork(`instagram`, `@Messi`);
+let NewPerson3 = new Person(`Eli`, `David`);
+let NewPerson4 = new Person(`Eli`, `Cohen`);
+let NewPerson5 = new Person(`Dani`, `Dani`);
 TwitterShlomo.addFollower(NewPerson3);
 TwitterShlomo.addFollower(NewPerson4);
 TwitterShlomo.addFollower(NewPerson5);
 instagramMessi.addFollower(NewPerson5);
 instagramMessi.addFollower(NewPerson4);
 console.log(TwitterShlomo);
-TwitterShlomo.removeFollower(" ");
+TwitterShlomo.removeFollower(` `);
 console.log(instagramMessi);
-instagramMessi.removeFollower(" ");
+instagramMessi.removeFollower(` `);
 instagramMessi.print();
