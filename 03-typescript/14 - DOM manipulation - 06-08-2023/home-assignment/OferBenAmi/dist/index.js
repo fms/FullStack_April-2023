229c357b3a382227081bb6fa89005f2c5f156c2e
OferBenAmi <benamiofer1@gmail.com>
2023-08-10 14:45:03 +0300

    continuing DOM assignment

--- 03-typescript/14 - DOM manipulation - 06-08-2023/home-assignment/OferBenAmi/dist/index.js
+++ 03-typescript/14 - DOM manipulation - 06-08-2023/home-assignment/OferBenAmi/dist/index.js
@@ -1,3 +1,17 @@
 "use strict";
 class Animal {
+    constructor(name, legs, eyes, predator) {
+        this.name = name;
+        this.legs = legs;
+        this.eyes = eyes;
+        this.predator = predator;
+    }
 }
+const animals = [
+    new Animal(`tiger`, 4, 2, true),
+    new Animal(`human`, 2, 2, true),
+    new Animal(`spider`, 8, 4, true)
+];
+let a = ``;
+let fullStr = animals.forEach(animal => `The name of the animal is ${animal.name}, it has ${animal.legs} legs and ${animal.predator ? `is` : `is not`} a predator, it has ${animal.eyes} eyes`);
+console.log(fullStr);
