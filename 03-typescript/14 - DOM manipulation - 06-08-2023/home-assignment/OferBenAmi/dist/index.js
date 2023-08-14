33f330697b58feb60b30c3479486190da6997123
OferBenAmi <benamiofer1@gmail.com>
2023-08-14 22:42:33 +0300

    Finishing class 14 DOM home assignment

--- 03-typescript/14 - DOM manipulation - 06-08-2023/home-assignment/OferBenAmi/dist/index.js
+++ 03-typescript/14 - DOM manipulation - 06-08-2023/home-assignment/OferBenAmi/dist/index.js
@@ -1,17 +1,33 @@
 "use strict";
 class Animal {
-    constructor(name, legs, eyes, predator) {
+    constructor(name, legs, eyes, predator, img) {
         this.name = name;
         this.legs = legs;
         this.eyes = eyes;
         this.predator = predator;
+        this.img = img;
     }
 }
 const animals = [
-    new Animal(`tiger`, 4, 2, true),
-    new Animal(`human`, 2, 2, true),
-    new Animal(`spider`, 8, 4, true)
+    new Animal(`tiger`, 4, 2, true, `imges/tiger.jpeg`),
+    new Animal(`human`, 2, 2, false, `imges/human.jpeg`),
+    new Animal(`spider`, 8, 4, true, `imges/spider.jpg`)
 ];
-let a = ``;
-let fullStr = animals.forEach(animal => `The name of the animal is ${animal.name}, it has ${animal.legs} legs and ${animal.predator ? `is` : `is not`} a predator, it has ${animal.eyes} eyes`);
-console.log(fullStr);
+const container = document.querySelector(`.container`);
+if (animals) {
+    animals.forEach(animal => {
+        const thisAnimalName = document.createElement("h1");
+        const thisAnimal = document.createElement(`h4`);
+        const thisAninalImg = document.createElement(`img`);
+        thisAnimalName.classList.add(`name-class`);
+        thisAnimal.classList.add(`description-class`);
+        thisAninalImg.classList.add(`img-class`);
+        thisAnimalName.textContent = animal.name;
+        thisAnimal.innerText = `The ${animal.name} is ${animal.predator ? `` : `not`} a predator, it has ${animal.legs} legs and has ${animal.eyes} eyes`;
+        thisAninalImg.src = animal.img;
+        thisAninalImg.style.width = `300px`;
+        container === null || container === void 0 ? void 0 : container.appendChild(thisAnimalName);
+        container === null || container === void 0 ? void 0 : container.appendChild(thisAninalImg);
+        container === null || container === void 0 ? void 0 : container.appendChild(thisAnimal);
+    });
+}
--- 03-typescript/14 - DOM manipulation - 06-08-2023/home-assignment/OferBenAmi/index.ts
+++ 03-typescript/14 - DOM manipulation - 06-08-2023/home-assignment/OferBenAmi/index.ts
@@ -3,16 +3,37 @@ class Animal {
     public name: string,
     public legs: number,
     public eyes: number,
-    public predator: boolean
+    public predator: boolean,
+    public img:string
   ) {}
 }
 
 const animals: Animal[] = [
-    new Animal(`tiger`, 4, 2,true),
-    new Animal(`human`,2,2,true),
-    new Animal(`spider`,8,4,true)
+    new Animal(`tiger`,4, 2,true, `imges/tiger.jpeg`),
+    new Animal(`human`,2,2,false, `imges/human.jpeg`),
+    new Animal(`spider`,8,4,true, `imges/spider.jpg`)
 
 ];
-let a = ``;
-let fullStr = animals.forEach(animal => `The name of the animal is ${animal.name}, it has ${animal.legs} legs and ${animal.predator ? `is`: `is not`} a predator, it has ${animal.eyes} eyes`);
-console.log(fullStr);
+
+const container = document.querySelector(`.container`);
+
+if(animals){
+  animals.forEach(animal => {
+    const thisAnimalName = document.createElement("h1");
+    const thisAnimal = document.createElement(`h4`);
+    const thisAninalImg = document.createElement(`img`);
+
+    thisAnimalName.classList.add(`name-class`);
+    thisAnimal.classList.add(`description-class`);
+    thisAninalImg.classList.add(`img-class`);
+
+    thisAnimalName.textContent = animal.name;
+    thisAnimal.innerText = `The ${animal.name} is ${animal.predator ? `` : `not`} a predator, it has ${animal.legs} legs and has ${animal.eyes} eyes`;
+    thisAninalImg.src = animal.img;
+    thisAninalImg.style.width = `300px`
+
+    container?.appendChild(thisAnimalName);
+    container?.appendChild(thisAninalImg)
+    container?.appendChild(thisAnimal);
+  })
+}
