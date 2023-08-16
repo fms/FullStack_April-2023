c4a87093b2a22d9f54d6b69531822629acadb89a
Bar .A <bar7569@gmail.com>
2023-08-16 20:38:50 +0300

    class practice

---
+++ 03-typescript/18 - intervals - 16-08-2023/class-assignment/Bar/index.ts
@@ -0,0 +1,15 @@
+let step = 500
+function move(element: HTMLElement) {
+    let left = element.offsetLeft;
+    let right = element.offsetLeft + left
+
+    if ((step > 0 && step + right > window.innerWidth) || (step < 0 && step + left < 0)) {
+        step = -step
+    }
+
+    element.style.position = "relative";
+    element.style.left = `${left + step}px`;
+}
+
+let div = document.querySelector("div")!;
+let moveTimerId = setInterval(move, 1000, div, 500);
