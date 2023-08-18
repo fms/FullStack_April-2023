c4e1ee424c7522353cc4f94a39f85ba23474270d
ofer ben ami <benamiofer1@gmail.com>
2023-08-18 04:52:02 +0300

    Fixing password assignment

--- 03-typescript/19 - events - 16-08-2023/home-assignment/OferBenAmi/Medium/password/dist/index.js
+++ 03-typescript/19 - events - 16-08-2023/home-assignment/OferBenAmi/Medium/password/dist/index.js
@@ -2,13 +2,6 @@
 let pass1 = document.querySelector('.pass1');
 let pass2 = document.querySelector('.pass2');
 const error = document.querySelector('.error');
-let pass1Value;
-let pass2Value;
-pass1 === null || pass1 === void 0 ? void 0 : pass1.addEventListener('input', (e) => {
-});
-pass2 === null || pass2 === void 0 ? void 0 : pass2.addEventListener('input', (e) => {
-    console.log(pass2.value);
-});
 setInterval(() => {
     if (pass1.value != pass2.value)
         error.style.opacity = `100%`;
