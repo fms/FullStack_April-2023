ff08f857c8dff55c901862276d8d2c99afcccd3b
ofer ben ami <benamiofer1@gmail.com>
2023-08-21 11:50:12 +0300

    making the code so that i can add as many buttons as i want

--- 03-typescript/19 - events - 16-08-2023/home-assignment/OferBenAmi/advanced/dist/index.js
+++ 03-typescript/19 - events - 16-08-2023/home-assignment/OferBenAmi/advanced/dist/index.js
@@ -2,23 +2,13 @@
 var _a, _b;
 const buttons = (_a = document.querySelector('.buttonWrapper')) === null || _a === void 0 ? void 0 : _a.querySelectorAll('.tab-button');
 const contents = (_b = document.querySelector('.contentWrapper')) === null || _b === void 0 ? void 0 : _b.querySelectorAll('.content');
-var buttonsAndContents;
-(function (buttonsAndContents) {
-    buttonsAndContents[buttonsAndContents["home"] = 0] = "home";
-    buttonsAndContents[buttonsAndContents["about"] = 1] = "about";
-    buttonsAndContents[buttonsAndContents["contact"] = 2] = "contact";
-})(buttonsAndContents || (buttonsAndContents = {}));
 buttons.forEach(button => {
     button.addEventListener('click', buttonClicked);
 });
 function buttonClicked(event) {
-    console.dir(event);
     let target = event.target;
-    buttons.forEach(button => {
-        button.classList.remove('active');
-    });
-    contents.forEach(content => {
-        content.classList.remove('active');
+    [...contents, ...buttons].forEach(something => {
+        something.classList.remove('active');
     });
     buttons.forEach((button, index) => {
         if (target.id == button.id) {
--- 03-typescript/19 - events - 16-08-2023/home-assignment/OferBenAmi/advanced/index.ts
+++ 03-typescript/19 - events - 16-08-2023/home-assignment/OferBenAmi/advanced/index.ts
@@ -1,27 +1,16 @@
 
 const buttons = document.querySelector('.buttonWrapper')?.querySelectorAll('.tab-button') as NodeListOf<HTMLButtonElement>
-const contents = document.querySelector('.contentWrapper')?.querySelectorAll('.content') as NodeListOf<HTMLButtonElement>
-
-enum buttonsAndContents {
-    home,
-    about,
-    contact
-}
+const contents = document.querySelector('.contentWrapper')?.querySelectorAll('.content') as NodeListOf<HTMLElement>
 
 buttons.forEach(button =>{
     button.addEventListener('click',buttonClicked)
 })
 
 function buttonClicked(event :MouseEvent){
-    console.dir(event)
-    let target = event.target as HTMLElement
+    let target = event.target as HTMLElement;
 
-    buttons.forEach(button =>{
-        button.classList.remove('active');
-    })
-
-    contents.forEach(content =>{
-        content.classList.remove('active');
+    [...contents,...buttons].forEach(something =>{
+        something.classList.remove('active');
     })
     buttons.forEach((button, index )=>{
         if(target.id == button.id){            
