bcbeeb3fd79df78fb5edbd58032fd1ef3318c8fe
Bar .A <bar7569@gmail.com>
2023-08-22 18:07:45 +0300

    Class puzzle optimization

--- 03-typescript/19 - events - 20-08-2023/class-puzzle/Bar/index.ts
+++ 03-typescript/19 - events - 20-08-2023/class-puzzle/Bar/index.ts
@@ -1,14 +1,25 @@
-let tds = document.querySelectorAll("td");
-let highlighted: HTMLElement;
+// let tds = document.querySelectorAll("td");
+// let highlighted: HTMLElement;
 
-tds.forEach(td => {
-    td.addEventListener("click", () => highlight(td));
-});
+// tds.forEach(td => {
+//     td.addEventListener("click", () => highlight(td));
+// });
 
-function highlight(td: any) {
-    if (highlighted) {
-        highlighted.style.background = "initial"
-    }
-    td.style.background = "yellow"
-    highlighted = td
-}
+// function highlight(td: any) {
+//     if (highlighted) {
+//         highlighted.style.background = "initial"
+//     }
+//     td.style.background = "yellow"
+//     highlighted = td
+// }
+
+let theTable = document.querySelector("table")
+let tds = document.querySelectorAll("td")
+
+theTable?.addEventListener("click", (event) => {
+    let target = event.target as HTMLElement
+    tds.forEach((td) => {
+        td.classList.remove("highlited")
+    })
+    target.classList.add("highlited")
+})
