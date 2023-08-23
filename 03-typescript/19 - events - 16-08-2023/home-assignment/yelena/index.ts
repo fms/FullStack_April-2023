657fa7ce9db2d08818e12803503ff375c3548efb
yelenaHayat <yelenamilhman@gmail.com>
2023-08-23 12:00:52 +0300

    update

---
+++ 03-typescript/19 - events - 16-08-2023/home-assignment/yelena/index.ts
@@ -0,0 +1,28 @@
+let mainBox = document.querySelector(".mainbox");
+let pinkBox = document.createElement("div");
+const defaultBox = pinkBox.style.backgroundColor
+
+pinkBox.classList.add("pinkBox");
+mainBox?.classList.add("mainBox")
+
+pinkBox.addEventListener("mouseenter", (ev) => {
+    if (ev.target instanceof HTMLElement) {
+        changeColor(ev.target, "white");
+    }
+    console.log(ev);
+})
+
+redBox.addEventListener("mouseleave", (ev) => {
+    if (ev.target instanceof HTMLElement) {
+        changeColor(ev.target, defaultColor);
+    }
+    console.log(ev);
+})
+
+function changeColor(element: HTMLElement, color: string) {
+    element.style.backgroundColor = color
+}
+
+if (mainBox) {
+    mainBox.appendChild(pinkBox);
+}
