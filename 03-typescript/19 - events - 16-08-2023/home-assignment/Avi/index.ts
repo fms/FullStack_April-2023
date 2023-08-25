20f63043cfa06e42ad83c1853bda7b1b66d36f89
AviVert <avremi6@gmail.com>
2023-08-25 17:19:37 +0300

    Dom home assignment

---
+++ 03-typescript/19 - events - 16-08-2023/home-assignment/Avi/index.ts
@@ -0,0 +1,63 @@
+let box: HTMLElement = document.querySelector('.main__box')!;
+
+box.addEventListener(`mouseover`, (event) =>
+    box.style.backgroundColor = `red`);
+box.addEventListener(`mouseout`, (event) =>
+    box.style.backgroundColor = `initial`);
+
+let img1 = 'https://shop-mobile.rami-levy.co.il/wp-content/uploads/2022/09/%D7%9C%D7%91%D7%9F-2.png';
+let img2 = 'https://cdn.awsli.com.br/600x700/1861/1861669/produto/190594792/apple-iphone-14-128-gb-roxo-3-agvoha.jpg';
+let img3 = 'https://shop-mobile.rami-levy.co.il/wp-content/uploads/2022/09/%D7%AA%D7%9B%D7%9C%D7%AA.png';
+
+
+let images = [img1, img2, img3];
+let imagenum = 0;
+
+let gallery: HTMLElement = document.querySelector('.main__gallery')!;
+let back: HTMLElement = document.querySelector('.back')!;
+let next: HTMLElement = document.querySelector('.next')!;
+gallery.style.backgroundSize = "310px 350px";
+gallery.style.backgroundImage = `url(${images[0]})`;
+
+
+next.addEventListener('click', (event: MouseEvent) => {
+    if (imagenum < images.length - 1) {
+        imagenum++;
+    }
+    else {
+        imagenum = 0;
+    }
+    gallery.style.backgroundImage = `url(${images[imagenum]})`;
+});
+
+
+back.addEventListener('click', (event: MouseEvent) => {
+    if (imagenum > 0) {
+        imagenum--;
+    }
+    else {
+        imagenum = images.length - 1;
+    }
+    gallery.style.backgroundImage = `url(${images[imagenum]})`;
+});
+
+let pass1: HTMLInputElement = document.querySelector('.pass1')!;
+let pass2: HTMLInputElement = document.querySelector('.pass2')!;
+let check: HTMLElement = document.querySelector('.main__chek')!;
+let result: HTMLElement = document.querySelector('.result')!;
+
+
+check.addEventListener('click', () => {    
+    const pass1_value = pass1.value;
+    const pass2_value = pass2.value;
+    if(pass1_value == pass2_value){
+        result.style.color = `green`;
+        result.innerHTML = 'good';
+
+    }
+    else{
+        result.style.color = `red`;
+        result.innerHTML = 'not good';
+    }
+})
+
