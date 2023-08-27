d0a518e468484623bb7468920cb82f0b79af303e
Shmuel Fishman <fms@shmulik-fishman.com>
2023-08-27 19:59:02 +0300

    Memory game is complete, using a class.

--- 03-typescript/21 - memory game/example/dist/index.js
+++ 03-typescript/21 - memory game/example/dist/index.js
@@ -1,62 +1,91 @@
 "use strict";
-const container = document.querySelector(".game-container");
-const dimension = 4;
-const images = [
-    "images/image-1.png",
-    "images/image-2.png",
-    "images/image-3.png",
-    "images/image-4.png",
-    "images/image-5.png",
-    "images/image-6.png",
-    "images/image-7.png",
-    "images/image-8.png",
-];
-const cards = [...images, ...images];
-console.log(container);
-cards.forEach((card) => createCard(container, card));
-function createCard(container, imageSrc) {
-    const card = document.createElement("div");
-    card.classList.add("item");
-    container.appendChild(card);
-    const image = document.createElement("img");
-    image.src = imageSrc;
-    card.appendChild(image);
-    card.addEventListener("click", (event) => cardFlip(event));
-}
-let firstCard = null;
-let secondCard = null;
-let counter = 0;
-function cardFlip(event) {
-    const card = event.target;
-    if (card.classList.contains("selected") ||
-        card.classList.contains("matched")) {
-        return;
+class MemoryGame {
+    constructor(container) {
+        this.container = container;
+        this.images = [
+            "images/image-1.png",
+            "images/image-2.png",
+            "images/image-3.png",
+            "images/image-4.png",
+            "images/image-5.png",
+            "images/image-6.png",
+            "images/image-7.png",
+            "images/image-8.png",
+        ];
+        this.firstCard = null;
+        this.counter = 0;
+        this.busy = false;
+        this.initCards();
     }
-    if (!firstCard) {
-        firstCard = card;
-        card.classList.add("selected");
-        return;
+    initCards() {
+        let cards = [...this.images, ...this.images];
+        this.randomizeCards(cards);
+        while (this.container.firstChild) {
+            this.container.firstChild.remove();
+        }
+        if (this.counter == 1) {
+            this.container.textContent = "You win!";
+        }
+        console.log(this.container);
+        this.counter = 0;
+        setTimeout(() => {
+            this.container.textContent = "";
+            cards.forEach((card) => this.createCard(this.container, card));
+        }, 1000);
     }
-    if (!secondCard) {
-        secondCard = card;
-        card.classList.add("selected");
-        if (firstCard.src === secondCard.src) {
-            console.log("match");
-            firstCard === null || firstCard === void 0 ? void 0 : firstCard.classList.remove("selected");
-            firstCard === null || firstCard === void 0 ? void 0 : firstCard.classList.add("matched");
-            secondCard === null || secondCard === void 0 ? void 0 : secondCard.classList.remove("selected");
-            secondCard === null || secondCard === void 0 ? void 0 : secondCard.classList.add("matched");
-            firstCard = null;
-            secondCard = null;
+    // this.initCards();
+    createCard(container, imageSrc) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.dataset.cardName = imageSrc;
+        container.appendChild(card);
+        const front = document.createElement("img");
+        front.classList.add("front");
+        front.src = imageSrc;
+        card.appendChild(front);
+        const back = document.createElement("div");
+        back.classList.add("back");
+        card.appendChild(back);
+        card.addEventListener("click", (event) => this.cardFlip(event));
+    }
+    cardFlip(event) {
+        const card = event.target;
+        if (this.busy || card.classList.contains("selected")) {
+            return;
+        }
+        if (!this.firstCard) {
+            this.firstCard = card;
+            card.classList.add("selected");
+            return;
+        }
+        const secondCard = card;
+        secondCard.classList.add("selected");
+        if (this.firstCard.dataset.cardName === secondCard.dataset.cardName) {
+            this.firstCard = null;
+            this.counter++;
+            if (this.counter == 1) {
+                this.initCards();
+            }
         }
         else {
-            console.log("Wrong");
+            this.busy = true;
             setTimeout(() => {
-                firstCard === null || firstCard === void 0 ? void 0 : firstCard.classList.remove("selected");
+                var _a;
+                (_a = this.firstCard) === null || _a === void 0 ? void 0 : _a.classList.remove("selected");
                 secondCard === null || secondCard === void 0 ? void 0 : secondCard.classList.remove("selected");
-                firstCard = null;
-                secondCard = null;
+                this.firstCard = null;
+                this.busy = false;
             }, 1000);
         }
     }
+    randomizeCards(cards) {
+        const len = cards.length;
+        // cards.sort(() => Math.random() > 0.5 ? 1 : -1);
+        for (let i = 0; i < len; i++) {
+            let randomIndex = Math.floor(Math.random() * (len - i));
+            [cards[i], cards[randomIndex]] = [cards[randomIndex], cards[i]];
+        }
+    }
 }
+const wrapper = document.querySelector(".game-container");
+const memoryGame = new MemoryGame(wrapper);
--- 03-typescript/21 - memory game/example/index.ts
+++ 03-typescript/21 - memory game/example/index.ts
@@ -1,70 +1,103 @@
-const container = document.querySelector(".game-container")!;
+class MemoryGame {
+    images = [
+        "images/image-1.png",
+        "images/image-2.png",
+        "images/image-3.png",
+        "images/image-4.png",
+        "images/image-5.png",
+        "images/image-6.png",
+        "images/image-7.png",
+        "images/image-8.png",
+    ];
+    firstCard: HTMLElement | null = null;
+    counter = 0;
+    busy = false;
 
-const dimension = 4;
-
-const images = [
-    "images/image-1.png",
-    "images/image-2.png",
-    "images/image-3.png",
-    "images/image-4.png",
-    "images/image-5.png",
-    "images/image-6.png",
-    "images/image-7.png",
-    "images/image-8.png",
-];
-const cards = [...images, ...images];
-console.log(container);
-cards.forEach((card) => createCard(container, card));
-
-function createCard(container: Element, imageSrc: string) {
-    const card = document.createElement("div");
-    card.classList.add("item");
-    container.appendChild(card);
-
-    const image = document.createElement("img");
-    image.src = imageSrc;
-    card.appendChild(image);
-
-    card.addEventListener("click", (event) => cardFlip(event));
-}
-
-let firstCard: HTMLImageElement | null = null;
-let secondCard: HTMLImageElement | null = null;
-let counter = 0;
-function cardFlip(event: MouseEvent) {
-    const card = event.target as HTMLImageElement;
-    if (card.classList.contains("selected") ||
-    card.classList.contains("matched"))
-    {
-        return;
+    constructor(private container: Element) {
+        this.initCards();
     }
 
-    if (!firstCard) {
-        firstCard = card;
-        card.classList.add("selected");
-        return;
-    }
-    
-    if (!secondCard) {
-        secondCard = card;
-        card.classList.add("selected");
+    initCards() {
+        let cards = [...this.images, ...this.images];
+        this.randomizeCards(cards);
 
-        if (firstCard.src === secondCard.src) {
-            console.log("match");
-            firstCard?.classList.remove("selected");
-            firstCard?.classList.add("matched");
-            secondCard?.classList.remove("selected");
-            secondCard?.classList.add("matched");
-            firstCard = null;
-            secondCard = null;
+        while (this.container.firstChild) {
+            this.container.firstChild.remove();
+        }
+        if (this.counter == 1) {
+            this.container.textContent = "You win!";
+        }
+
+        console.log(this.container);
+        this.counter = 0;
+        setTimeout(() => {
+            this.container.textContent = "";
+            cards.forEach((card) => this.createCard(this.container, card));
+        }, 1000);
+    }
+
+    // this.initCards();
+
+    createCard(container: Element, imageSrc: string) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.dataset.cardName = imageSrc;
+        container.appendChild(card);
+
+        const front = document.createElement("img");
+        front.classList.add("front");
+        front.src = imageSrc;
+        card.appendChild(front);
+
+        const back = document.createElement("div");
+        back.classList.add("back");
+        card.appendChild(back);
+
+        card.addEventListener("click", (event) => this.cardFlip(event));
+    }
+
+    cardFlip(event: MouseEvent) {
+        const card = event.target as HTMLElement;
+
+        if (this.busy || card.classList.contains("selected")) {
+            return;
+        }
+
+        if (!this.firstCard) {
+            this.firstCard = card;
+            card.classList.add("selected");
+            return;
+        }
+
+        const secondCard = card;
+        secondCard.classList.add("selected");
+
+        if (this.firstCard.dataset.cardName === secondCard.dataset.cardName) {
+            this.firstCard = null;
+            this.counter++;
+            if (this.counter == 1) {
+                this.initCards();
+            }
         } else {
-            console.log("Wrong");
+            this.busy = true;
             setTimeout(() => {
-                firstCard?.classList.remove("selected");
+                this.firstCard?.classList.remove("selected");
                 secondCard?.classList.remove("selected");
-                firstCard = null;
-                secondCard = null;
+                this.firstCard = null;
+                this.busy = false;
             }, 1000);
         }
     }
+
+    randomizeCards(cards: string[]) {
+        const len = cards.length;
+        // cards.sort(() => Math.random() > 0.5 ? 1 : -1);
+        for (let i = 0; i < len; i++) {
+            let randomIndex = Math.floor(Math.random() * (len - i));
+            [cards[i], cards[randomIndex]] = [cards[randomIndex], cards[i]];
+        }
+    }
 }
+
+const wrapper = document.querySelector(".game-container")!;
+const memoryGame = new MemoryGame(wrapper);
