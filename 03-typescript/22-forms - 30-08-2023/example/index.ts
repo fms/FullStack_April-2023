b81cfacde6334f2511ab7e40116392ac4b10e96f
Shmuel Fishman <fms@shmulik-fishman.com>
2023-08-30 21:00:03 +0300

    Example extended for forms

--- 03-typescript/22-forms - 30-08-2023/example/index.ts
+++ 03-typescript/22-forms - 30-08-2023/example/index.ts
@@ -4,9 +4,30 @@ function handleClick(event: MouseEvent) {
 
 const validValues = [ "Hello", "There"];
 
-function handleKeyup(event: KeyboardEvent) {
+function printValue(event: KeyboardEvent) {
     const target: HTMLInputElement | null = event.target;
     if (target) {
+        console.log(`key up ${target.value} ${target.valueAsNumber}`);
+    }
+}
+
+function submitForm(event: SubmitEvent) {
+    console.dir(event);
+    const elements = (event.target as HTMLFormElement)!.elements;
+    // @ts-ignore
+    const text = elements.f1.value;
+    // const number = event.target[1].valueAsNumber;
+    // @ts-ignore
+    const number = elements.numeric.valueAsNumber;
+    console.log(`Text: "${text}" number: "${number}"`);
+    
+    return false;
+}
+
+function handleKeyup(event: KeyboardEvent) {
+    const target = event.target as HTMLInputElement;
+    if (target.tagName === "INPUT")
+     {
         console.log(`key up ${event.key} ${target.value.length}`);
         const p = document.querySelector("#error-text");
         if (p) {
@@ -14,6 +35,10 @@ function handleKeyup(event: KeyboardEvent) {
             // p.textContent = validValues.indexOf(target.value) === -1 ?
             //          "Bad value" : "";
         }
+    }
+    else {
+        console.log(`key up ${event.key}`);
+
     }
 }
 
