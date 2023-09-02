d725f9f57d66824ba4ec44b3733b4f6304941674
ashours <shlomiasus257@gmail.com>
2023-09-02 12:35:49 +0300

    checkbox

--- shlomiAsus/checkbox/dist/index.js
+++ shlomiAsus/checkbox/dist/index.js
@@ -5,14 +5,18 @@ checkBoxes.forEach(function (checkBox) {
     checkBox.addEventListener("click", function () { return print(checkBox); });
 });
 function print(checkBox) {
+    // @ts-ignore
     myDiv.innerHTML += " " + checkBox.value;
+    // @ts-ignore
     submit += " " + checkBox.value;
     console.log(submit);
 }
 function submitForm() {
     var whatIsOn = "";
     checkBoxes.forEach(function (checkBox) {
+        // @ts-ignore
         if (checkBox.checked) {
+            // @ts-ignore
             whatIsOn += checkBox.value + ",";
         }
     });
