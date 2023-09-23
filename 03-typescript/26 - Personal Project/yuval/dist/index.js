885de64b4b8d93739cb7105b46260cdd17462ee6
uv1908 <yuval.lavi@avilavi.net>
2023-09-23 15:35:06 +0300

    trying to save in localStorage

--- 03-typescript/26 - Personal Project/yuval/dist/index.js
+++ 03-typescript/26 - Personal Project/yuval/dist/index.js
@@ -3,12 +3,11 @@ const form1div = document.getElementById('newWatchListDiv');
 const form2div = document.getElementById('watchListDiv');
 const select = document.getElementById('selectList');
 const form1 = document.getElementById('start-form');
-const form2 = document.getElementById('add-movies');
-class WatchList {
-    constructor(listName) {
-        this.listName = listName;
-    }
-}
+const form2 = document.getElementById('add-savedMovies');
+const newListButton = document.getElementById('addNewList');
+let fieldName = "movies";
+let movies = loadMovies();
+form1.addEventListener('submit', createList);
 function createList(ev) {
     ev.preventDefault();
     const name = document.getElementById('listNames').value;
@@ -19,7 +18,36 @@ function createList(ev) {
     form1div.classList.toggle('hide');
     form2div.classList.toggle('hide');
 }
-form1.addEventListener('submit', createList);
+newListButton.addEventListener('click', () => {
+    form1div.classList.toggle('hide');
+    form2div.classList.toggle('hide');
+    form1.reset();
+});
+function formSubmit(ev) {
+    ev.preventDefault();
+    const elements = form2.elements;
+    const watchList = new WatchList(elements.listName.value);
+    const newMovie = new Movie(elements.movieName.value, elements.year.valueAsNumber);
+    let newWatchListMovie = new WatchListMovie(watchList, newMovie);
+    movies.push(newWatchListMovie);
+    localStorage.setItem('movies', JSON.stringify(movies));
+}
+function saveMovies() {
+    const moviesStringified = JSON.stringify(movies);
+    localStorage.setItem(fieldName, moviesStringified);
+}
+function loadMovies() {
+    const savedMovies = localStorage.getItem(fieldName);
+    if (savedMovies) {
+        return JSON.parse(savedMovies);
+    }
+    return new Array();
+}
+class WatchList {
+    constructor(listName) {
+        this.listName = listName;
+    }
+}
 class Movie {
     constructor(movieName, year, seen = false) {
         this.movieName = movieName;
--- 03-typescript/26 - Personal Project/yuval/index.ts
+++ 03-typescript/26 - Personal Project/yuval/index.ts
@@ -1,16 +1,27 @@
 interface HTMLSelectElementWithId extends HTMLSelectElement {
     add(option: HTMLOptionElement, before?: HTMLElement | null): void;
 }
+
+interface FormElements extends HTMLFormControlsCollection {
+    listName: HTMLInputElement;
+    movieName: HTMLInputElement;
+    year: HTMLInputElement;
+    add: HTMLInputElement;
+    update: HTMLInputElement;
+    cancel: HTMLInputElement;
+}
+
 const form1div = document.getElementById('newWatchListDiv') as HTMLElement;
 const form2div = document.getElementById('watchListDiv') as HTMLElement;
 const select = document.getElementById('selectList') as HTMLSelectElementWithId;
 const form1 = document.getElementById('start-form') as HTMLFormElement;
-const form2 = document.getElementById('add-movies') as HTMLFormElement;
+const form2 = document.getElementById('add-savedMovies') as HTMLFormElement;
+const newListButton = document.getElementById('addNewList') as HTMLButtonElement;
 
-class WatchList {
-    constructor(public listName: string){}
+let fieldName = "movies";
+let movies = loadMovies();
 
-}
+form1.addEventListener('submit', createList);
 
 function createList(ev: Event){
     ev.preventDefault();
@@ -23,8 +34,41 @@ function createList(ev: Event){
     form2div.classList.toggle('hide');
 }
 
-form1.addEventListener('submit', createList);
+newListButton.addEventListener('click', () => {
+    form1div.classList.toggle('hide');
+    form2div.classList.toggle('hide');
+    form1.reset();
+});
 
+function formSubmit(ev: SubmitEvent){
+    ev.preventDefault();
+    const elements = form2.elements as FormElements;
+    const watchList = new WatchList(elements.listName.value);
+    const newMovie = new Movie(elements.movieName.value, elements.year.valueAsNumber);
+    let newWatchListMovie = new WatchListMovie(watchList, newMovie);
+    movies.push(newWatchListMovie);
+    localStorage.setItem('movies', JSON.stringify(movies));
+}
+
+
+function saveMovies() {
+    const moviesStringified = JSON.stringify(movies);
+    localStorage.setItem(fieldName, moviesStringified);
+}
+
+function loadMovies(): WatchListMovie[] {
+    const savedMovies = localStorage.getItem(fieldName);
+    if (savedMovies) {
+        return JSON.parse(savedMovies);
+    }
+
+    return new Array<WatchListMovie>();
+}
+
+class WatchList {
+    constructor(public listName: string){}
+
+}
 class Movie {
     constructor(public movieName: string, public year: number, public seen: boolean = false){}
 }
