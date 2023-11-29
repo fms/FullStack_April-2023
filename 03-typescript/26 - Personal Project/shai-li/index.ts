cf70a1f030565050e240274ddc20e5937062db79
Shai-li Harniv <shaili6298@gmail.com>
2023-11-29 13:26:07 +0200

    G

--- 03-typescript/26 - Personal Project/shai-li/index.ts
+++ 03-typescript/26 - Personal Project/shai-li/index.ts
@@ -3,10 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const calendarDays = document.querySelector(".day");
   const prevWeekButton = document.getElementById("prevWeek");
   const nextWeekButton = document.getElementById("nextWeek");
-  
+
   let startDate = new Date();
   updateCalendar(startDate);
-  
 
   prevWeekButton!.addEventListener("click", () => {
     startDate.setDate(startDate.getDate() - 7);
@@ -18,27 +17,23 @@ document.addEventListener("DOMContentLoaded", () => {
     updateCalendar(startDate);
   });
 
-  
-
   function updateCalendar(start: Date) {
     currentDate.textContent = getFormattedWeekRange(startDate);
     calendarDays!.innerHTML = "";
-    
+
     for (let i = 0; i < 7; i++) {
       const day = new Date(start);
       // console.log(day);
       day.setDate(start.getDate() + i); //מעלה את הימים
-      const dayElement = document.createElement("span"); 
+      const dayElement = document.createElement("span");
       dayElement.className = "day";
-      
+
       dayElement.innerHTML = `${day.getDate()}`;
-      
+
       calendarDays!.appendChild(dayElement);
-      
+
       return;
-      
     }
-    
   }
 
   function getFormattedWeekRange(startDate: Date) {
@@ -46,5 +41,4 @@ document.addEventListener("DOMContentLoaded", () => {
     endDate.setDate(startDate.getDate() + 6);
     return `${startDate.toDateString()} - ${endDate.toDateString()}`;
   }
-
 });
