0a359cf9bf4e916fab709ebdf887d4d2e0128fb6
Saarxp <saar772@gmail.com>
2023-12-20 21:12:28 +0200

    04-03-saar israeli

--- 04-nodeJS/03 - typescript, async - 20-12-2023/class-assignment/Saar israeli/public/index.ts
+++ 04-nodeJS/03 - typescript, async - 20-12-2023/class-assignment/Saar israeli/public/index.ts
@@ -1,11 +1,4 @@
 let titleButton = document.querySelector(".header-button") as HTMLButtonElement;
-// let titleHeader = document.querySelector(".header-container-title") as HTMLParagraphElement;
-
-// const colors = ["red","blue","yellow","green"];
-
-// function randomColor() {
-//     colors.splice(Math.floor(Math.random() * (colors.length - 1)))
-// }
 
 if(titleButton) {
     titleButton.addEventListener('click', (event) => {
