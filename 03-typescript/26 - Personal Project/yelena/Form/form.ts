9e308c8fabc871a904d19c451b84f457b0217f3e
yelenaHayat <yelenamilhman@gmail.com>
2023-12-20 20:05:12 +0200

    updates

--- 03-typescript/26 - Personal Project/yelena/Form/form.ts
+++ 03-typescript/26 - Personal Project/yelena/Form/form.ts
@@ -1,47 +1,53 @@
+console.log("shalom");
 
-interface FormObject extends HTMLFormControlsCollection {
-    name: HTMLInputElement;
-    email: HTMLInputElement;
-    age: HTMLInputElement;
-    classes: HTMLInputElement;
-    phone: HTMLInputElement;
-    update: HTMLInputElement;
-    cancel: HTMLInputElement;
+interface FormObject {
+  name: string;
+  email: string;
+  age: number| string;
+  classes: string;
+  phone: string;
 }
 
 const form = document.querySelector<HTMLFormElement>("#form");
 if (form) {
-    form.addEventListener("submit", function (event) {
-        // Prevent the default form submission behavior
-        event.preventDefault();
-        debugger;
-
-        
-        const nameField = document.getElementById("name") as HTMLInputElement;
-        const emailField = document.getElementById("email") as HTMLInputElement;
-        const ageField = document.getElementById("age") as HTMLInputElement;
-        const classesField = document.getElementById("classes") as HTMLInputElement;
-        const phoneField =  document.getElementById("phone") as HTMLInputElement;
-
-        const name = nameField ? nameField.value : "";
-        const email = emailField ? emailField.value : "";
-        const age = ageField ? ageField.value : "";
-        const classes = classesField ? classesField.value : "";
+  const nameField = document.getElementById("name") as HTMLInputElement;
+  const emailField = document.getElementById("email") as HTMLInputElement;
+  const ageField = document.getElementById("age") as HTMLInputElement;
+  const classesField = document.getElementById("classes") as HTMLInputElement;
+  const phoneField = document.getElementById("phone") as HTMLInputElement;
 
 
-        /**
-         * Create an Object with all the data
-         * the properties in the object should be like the interface above
-         * the object will be of type FormObject
-         */
-        // const formData: FormObject = {}
+  form.addEventListener("submit", function (event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    const name = nameField ? nameField.value : "";
+    const email = emailField ? emailField.value : "";
+    const age = ageField ? +ageField.value : "";
+    const classes = classesField ? classesField.value : "";
+    const phone = phoneField ? phoneField.value : "";
 
-        /**
-         * Adding the object to localStorage
-         * I gave you an example in WhatsApp
-         */
+    const fromData: FormObject = {
+        name, 
+        email,
+        age,
+        classes,
+        phone,
+    }
 
+    debugger;
+    
+    /**
+     * Create an Object with all the data
+     * the properties in the object should be like the interface above
+     * the object will be of type FormObject
+     */
+    // const formData: FormObject = {}
 
+    /**
+     * Adding the object to localStorage
+     * I gave you an example in WhatsApp
+     */
+  });
 
-    });
+local storage set item
 }
