330c5fe41d5f211711219a1d6141ba9e89cd5770
Shmuel Fishman <fms@shmulik-fishman.com>
2023-12-20 19:56:42 +0200

    Use node.js with typescript directly (ts-node setup)

---
+++ 04-nodeJS/03 - typescript, async - 20-12-2023/typescript/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
