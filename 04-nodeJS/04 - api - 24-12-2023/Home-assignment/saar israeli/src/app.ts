8774ef7615d4ffc6e7268bcf858e32458252c03a
Saarxp <saar772@gmail.com>
2023-12-27 00:23:53 +0200

    fixing now on home-assignment 04/04

--- 04-nodeJS/04 - api - 24-12-2023/Home-assignment/saar israeli/src/app.ts
+++ 04-nodeJS/04 - api - 24-12-2023/Home-assignment/saar israeli/src/app.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import fs from 'fs';
-import liveDate from '../components/time'
+// import liveDate from '../components/time'
 
-const israelCurrentDate = liveDate('Asia/Jerusalem');
+// const israelCurrentDate = liveDate('Asia/Jerusalem');
 const app = express();
 const options = process.cwd();
 const port = 3000;
@@ -15,7 +15,7 @@ res.send({message: req.params.text});
 })
 
 app.get('/now', (req, res) => {
-    res.send({date: israelCurrentDate});
+    res.sendFile('./public/now.html', {root: options});
 })
 
 // app.use((req,res) => {
