ac69eef367bbb7b5ac39fdec71a08be7c621f092
Bar .A <bar7569@gmail.com>
2024-01-03 21:39:42 +0200

    added delete button with functionality

--- 04-nodeJS/06 - api - 31-12-2023/home-assignment/Bar/assignment/public/index.ts
+++ 04-nodeJS/06 - api - 31-12-2023/home-assignment/Bar/assignment/public/index.ts
@@ -27,30 +27,68 @@ async function getProducts() {
 
 // Update the page's product list
 function renderProducts(element: HTMLDivElement, products: Product[]) {
-  element.innerHTML =
-    "<h1>Products</h1>" +
-    products
-      .map((product) => {
-        return `<p>${product.name}, price: ${product.price}</p>`;
-      })
-      .join("<hr>");
+  element.innerHTML = "";
+
+  products.forEach((product) => {
+    const productOutputDiv = document.createElement("div") as HTMLDivElement;
+    const productDetails = document.createElement("p") as HTMLParagraphElement;
+    const deleteButton = document.createElement("button") as HTMLButtonElement;
+    const updateButton = document.createElement("button") as HTMLButtonElement;
+
+    productDetails.textContent = `${product.name}, ${product.price}`;
+    deleteButton.textContent = "Delete";
+    updateButton.textContent = "Update";
+
+    deleteButton.addEventListener("click", () => deleteProduct(product.name));
+
+    productOutputDiv.style.border = "1px solid blue";
+    productOutputDiv.appendChild(productDetails);
+    productOutputDiv.appendChild(deleteButton);
+    productOutputDiv.appendChild(updateButton);
+    element.appendChild(productOutputDiv);
+  });
 }
 
-async function handleAddProduct(event:MouseEvent) {
-    event.preventDefault();
-    
-    const productName = document.querySelector(".productName") as HTMLInputElement;
-    const productPrice = document.querySelector(".productPrice") as HTMLInputElement;
+async function handleAddProduct(event: MouseEvent) {
+  event.preventDefault();
+
+  const productName = document.querySelector(".productName") as HTMLInputElement;
+  const productPrice = document.querySelector(".productPrice") as HTMLInputElement;
+
   try {
     const newProduct: Product = {
       name: productName.value,
       price: Number(productPrice.value),
     };
+
     const response = await fetch("/api/products/product", {
       method: "POST",
       headers: { "content-type": "application/json" },
       body: JSON.stringify(newProduct),
     });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    // console.log(data);
+    renderProducts(rootElement, data.products);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+}
+async function deleteProduct(name:string) {
+
+  try {
+    const response = await fetch("/api/products/product", {
+      method: "DELETE",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({name}),
+    });
+
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
