ced17e5277b431adbcd252c96a8376664b6fcb35
OferBenAmi <benamiofer1@gmail.com>
2024-01-07 15:06:38 +0200

    starting homework, not finished

---
+++ 03-typescript/26 - Personal Project/OferBenAmi-AP Project/typescript/functions/pushRow.ts
@@ -0,0 +1,35 @@
+// import { apDetailsEntry } from "../classes/classesAndInterfaces";
+
+// export function pushRow(submittedApDetails: apDetailsEntry) {
+// 	const apTable = document.querySelector(".apTable");
+
+// 	const newRow = document.createElement("tr");
+
+// 	const vendorNametd = document.createElement("td");
+// 	const modelNametd = document.createElement("td");
+// 	const hostNametd = document.createElement("td");
+// 	const macAddrtd = document.createElement("td");
+// 	const ipAddrtd = document.createElement("td");
+// 	const Versiontd = document.createElement("td");
+// 	const deletetd = document.createElement("td");
+
+// 	const deleteCheckBox = document.createElement("input");
+// 	deleteCheckBox.setAttribute("type", "checkBox");
+// 	deletetd.appendChild(deleteCheckBox);
+
+// 	vendorNametd.textContent = submittedApDetails.vendorName;
+// 	modelNametd.textContent = submittedApDetails.modelName;
+// 	hostNametd.textContent = submittedApDetails.hostName;
+// 	macAddrtd.textContent = submittedApDetails.macAddr;
+// 	ipAddrtd.textContent = submittedApDetails.ipAddr;
+// 	Versiontd.textContent = submittedApDetails.Version;
+
+// 	newRow.appendChild(vendorNametd);
+// 	newRow.appendChild(modelNametd);
+// 	newRow.appendChild(hostNametd);
+// 	newRow.appendChild(macAddrtd);
+// 	newRow.appendChild(ipAddrtd);
+// 	newRow.appendChild(Versiontd);
+// 	newRow.appendChild(deletetd);
+// 	apTable?.appendChild(newRow);
+// }
