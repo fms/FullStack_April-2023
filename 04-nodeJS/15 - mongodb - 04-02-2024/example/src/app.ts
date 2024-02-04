d52453b69bb3c0e22be1ffaea2e11caa9f581e2d
Shmuel Fishman <fms@shmulik-fishman.com>
2024-02-04 19:39:19 +0200

    Refactor: Move database code to its own module. Also added default toObject() to map _id to id.

--- 04-nodeJS/15 - mongodb - 04-02-2024/example/src/app.ts
+++ 04-nodeJS/15 - mongodb - 04-02-2024/example/src/app.ts
@@ -1,13 +1,11 @@
+import { connectToDatabase, setDatabaseDefaults } from './database';
 import createServer from './server';
 import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
 const app = createServer();
 
-async function connectToMongoDB() {
-    await mongoose.connect('mongodb://localhost/todo');
-}
-
-connectToMongoDB();
+connectToDatabase();
+setDatabaseDefaults();
 app.listen(port, () => console.log(`Server started on http://localhost:${port}`));
 
