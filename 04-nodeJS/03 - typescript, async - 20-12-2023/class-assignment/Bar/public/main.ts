0f43c4a5e0866524f71b61add13aee0274529568
yelenaHayat <yelenamilhman@gmail.com>
2024-02-10 09:40:21 +0200

    updates

---
+++ 04-nodeJS/03 - typescript, async - 20-12-2023/class-assignment/Bar/public/main.ts
@@ -0,0 +1,19 @@
+const main = document.querySelector(".main") as HTMLDivElement;
+const div = document.createElement("div") as HTMLDivElement;
+const h2 = document.createElement("h2") as HTMLElement;
+const input = document.createElement("input") as HTMLInputElement;
+const input2 = document.createElement("input") as HTMLInputElement;
+const button = document.createElement("button") as HTMLButtonElement;
+
+h2.textContent = "Enter some text";
+button.textContent = "Click";
+
+button.addEventListener("click", (event: MouseEvent) => {
+    console.log(event);
+})
+
+div.append(h2);
+div.append(button);
+div.append(input);
+div.append(input2);
+main.append(div);
