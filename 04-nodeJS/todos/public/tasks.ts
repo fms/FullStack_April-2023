9d03e39852f3463426a9ba1b670e0042ffc5d502
Shmuel Fishman <fms@shmulik-fishman.com>
2024-03-03 18:51:16 +0200

    Add missing functionality to the frontend.

--- 04-nodeJS/todos/public/tasks.ts
+++ 04-nodeJS/todos/public/tasks.ts
@@ -10,26 +10,33 @@ interface Task {
     status: TaskStatus;
 }
 
-//get tasks from server
+
+// Initial load of data
+const tasksElement: HTMLDivElement | null = document.querySelector('#tasks');
+const errorsElement: HTMLDivElement = document.querySelector('#errors')!;
+handleGetTasks();
+
+// Common response handling (and get tasks) from server
 async function processResponse(response: Response) {
     try {
         if (!response.ok) {
             throw new Error(response.statusText)
         }
 
+        errorsElement.innerText = "";
         const { tasks }: { tasks: Task[] } = await response.json();
         renderTasks(tasks, tasksElement);
     } catch (error) {
         if (error instanceof Error) {
-            console.error(error.message);
+            errorsElement.innerText = error.message;
+            console.error("Error: ", error.message);
         }
         else {
-            console.error(error);
+            errorsElement.innerText = error as string;
+            console.error("Error: ", error);
         }
     }
 }
-
-const tasksElement: HTMLDivElement | null = document.querySelector('#tasks');
 
 async function handleGetTasks() {
     const response = await fetch('/api/tasks/get');
@@ -41,7 +48,7 @@ async function handleAddTask(event: SubmitEvent) {
     event.preventDefault();
 
     // Get form fields
-    const formData = new FormData(event.currentTarget as HTMLFormElement);
+    const formData = new FormData(event.target as HTMLFormElement);
     const title = formData.get('title');
     const description = formData.get('description');
 
@@ -54,6 +61,19 @@ async function handleAddTask(event: SubmitEvent) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(newTask)
+    });
+
+    await processResponse(response);
+}
+
+async function handleDelete(id: string) {
+    const body = { id };
+    const response = await fetch('/api/tasks/delete', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
     });
 
     await processResponse(response);
@@ -72,16 +92,25 @@ async function handleUpdateStatus(status: TaskStatus, id: string) {
     await processResponse(response);
 }
 
-function renderTask(task: Task) {
+function renderTask(body: HTMLDivElement, task: Task) {
     try {
-        const html = task.status === TaskStatus.todo
-            ? `<li onclick="handleUpdateStatus('done', '${task.id}')">${task.title} - ${task.description}</li>`
-            :
-            `<li style="text-decoration: line-through;" onclick="handleUpdateStatus('todo', '${task.id}')">${task.title} - ${task.description}</li>`;
-        return html;
+        const title = createElement("div", "task", task.title);
+        const description = createElement("div", "task", task.description);
+
+        if (task.status === TaskStatus.done) {
+            title.classList.add("task-done");
+            description.classList.add("task-done");
+        }
+        
+        body.appendChild(title);
+        body.appendChild(description);
+
+        const checkbox = createInputElement("checkbox", "checkbox", () => handleUpdateStatus(task.status === TaskStatus.done ? TaskStatus.todo : TaskStatus.done, task.id));
+        checkbox.checked = (task.status === TaskStatus.done);
+        body.appendChild(checkbox);
+        body.appendChild(createInputElement("button", "task", () => handleDelete(task.id), "Delete"));
     } catch (error) {
         console.error(error);
-        return "";
     }
 }
 
@@ -91,12 +120,41 @@ function renderTasks(tasks: Task[], div: HTMLDivElement | null) {
             throw new Error("no div element");
         }
 
-        let html = "<ul>";
-        html += tasks.map(task => renderTask(task)).join("");
-        html += "</ul>";
-
-        div.innerHTML = html;
+        div.replaceChildren();
+        if (tasks.length !== 0) {
+            div.append(...createHeader());
+            tasks.forEach(task => renderTask(div, task));
+        }
     } catch (error) {
         console.error(error);
     }
+}
+
+function createElement(tagName: string, className: string, content: string): HTMLElement {
+    const element = document.createElement(tagName);
+    element.className = className;
+    element.textContent = content;
+    return element;
+}
+
+function createInputElement(inputType: string, className: string, handler: ((event: Event) => void) | null, content: string = ""): HTMLInputElement {
+    const element = document.createElement("input");
+    element.type = inputType;
+    element.className = className;
+    if (handler) {
+        element.addEventListener("click", handler);
+    }
+    if (content) {
+        element.value = content;
+    }
+    return element;
+}
+
+function createHeader(): Node[] {
+    return [
+        createElement("div", "header", "Title"),
+        createElement("div", "header", "Description"),
+        createElement("div", "header", "Done?"),
+        createElement("div", "header", "")
+    ];
 }
