4a00b4ee866f2064fb7c62e627b76438f042375b
Bar .A <bar7569@gmail.com>
2024-03-05 20:34:56 +0200

    class assignment

--- 05-react/01 - intro - 03-03-2024/class-assignment/Bar/my-app/src/App.tsx
+++ 05-react/01 - intro - 03-03-2024/class-assignment/Bar/my-app/src/App.tsx
@@ -1,11 +1,30 @@
-import React, { ChangeEvent, useState } from "react"
-import './App.css';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import "./dist/styles.css"
+interface Human {
+  id: string
+  name: string,
+  lastName: string
+}
 
 function App() {
+  const [peopleList, setPeopleList] = useState<Human[]>([]);
   const [name, setName] = useState("")
+  const [lastName, setLastName] = useState("")
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    setName(event.target.value)
+    if (event.target.className === "name") {
+      setName(event.target.value)
+    }
+    else if (event.target.className === "lastName") {
+      setLastName(event.target.value)
+    }
+  }
+
+  function handleSubmit(event: FormEvent<HTMLButtonElement>) {
+    event.preventDefault();
+
+    const newHuman: Human = { id: crypto.randomUUID(), name: name, lastName: lastName }
+    setPeopleList([...peopleList, newHuman])
   }
 
   return (
@@ -13,10 +32,23 @@ function App() {
       <form>
         <label>
           Name:
-          <input onChange={event => handleChange(event)} placeholder="Name" type="text" />
+          <input onChange={event => handleChange(event)} type="text" className='name' placeholder='Name' />
         </label>
-        <h1>{name}</h1>
+        <label> Last Name:
+          <input onChange={event => handleChange(event)} type="text" className='lastName' placeholder='Last Name' />
+        </label>
+        <button onClick={(event) => handleSubmit(event)} type='button'>Add</button>
       </form>
+      <h1>People:</h1>
+      <div >
+        {peopleList.map((human) => {
+          return (<div className='people' key={human.id}>
+            <p>{human.name}</p>
+            <p>{human.lastName}</p>
+          </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
