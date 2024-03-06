bb36293d504172bc96bf9eaa42c8276be3860c05
Shmuel Fishman <fms@shmulik-fishman.com>
2024-03-07 01:57:58 +0200

    Updated the example to use file path aliases.

--- 05-react/02 - components - 06-03-2024/example-vite-react/src/components/Counter/Counter.tsx
+++ 05-react/02 - components - 06-03-2024/example-vite-react/src/components/Counter/Counter.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "../../assets/react.svg";
+import logo from "@assets/react.svg";
 import colors from "./counter.module.scss";
 
 function Counter() {
