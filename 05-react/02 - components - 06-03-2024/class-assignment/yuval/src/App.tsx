88279639fbf05485f8e2278d404f126182aaa8e5
uv1908 <yuval.lavi@avilavi.net>
2024-03-06 20:56:09 +0200

    added a svg icon and deleted useless code

--- 05-react/02 - components - 06-03-2024/class-assignment/yuval/src/App.tsx
+++ 05-react/02 - components - 06-03-2024/class-assignment/yuval/src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Article from './Article'
 import './App.css'
 
 function App() {
     return (
         <>
-        <Article />
+            <Article />
         </>
     )
 }
