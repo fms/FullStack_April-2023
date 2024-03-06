1e8012ba4e3bcdc0e34247f9128bfccb8fc40a35
uv1908 <yuval.lavi@avilavi.net>
2024-03-06 19:54:19 +0200

    class assignment on react components

---
+++ 05-react/02 - components - 06-03-2024/class-assignment/yuval/src/Article.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import cody from './assets/cody.png'
+
+function Article() {
+    return (
+        <>
+        <div>
+            <h3>Cody Rhodes responds to The Rock</h3>
+            <div className='article'>
+                <img src={cody} className='cody' />
+                <p>Cody Rhodes warns The Rock not to talk his about dog ever again 👀 <br />
+                The Rock previously referred to Cody’s dog as a “goofy ass dog”</p>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Article;
