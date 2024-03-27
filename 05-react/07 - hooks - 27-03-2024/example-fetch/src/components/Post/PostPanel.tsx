94b123970ad419c7f4cac189cf524a917c7ec214
Shmuel Fishman <fms@shmulik-fishman.com>
2024-03-27 21:35:20 +0200

    Updated Post to include a loading... message

--- 05-react/07 - hooks - 27-03-2024/example-fetch/src/components/Post/PostPanel.tsx
+++ 05-react/07 - hooks - 27-03-2024/example-fetch/src/components/Post/PostPanel.tsx
@@ -6,14 +6,19 @@ interface PostProps {
 
 export default function PostPanel({ id }: PostProps) {
   const [post, setPost] = useState<Post>();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
     fetch(`https://dummyjson.com/posts/${id}`, { signal: controller.signal })
       .then((res) => res.json())
       .then((obj) => {
         setPost(obj);
+
+        // We can't use .finally, as it will also be called after an abort.
+        setIsLoading(false);
       });
     return () => {
       controller.abort();
@@ -21,11 +26,17 @@ export default function PostPanel({ id }: PostProps) {
   }, [id]);
 
   return (
-    <>
-      <h1>
-        {post?.title} {post?.id}
-      </h1>
-      <p>{post?.body}</p>
-    </>
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1>
+            {post?.title} {post?.id}
+          </h1>
+          <p>{post?.body}</p>
+        </>
+      )}
+    </div>
   );
 }
